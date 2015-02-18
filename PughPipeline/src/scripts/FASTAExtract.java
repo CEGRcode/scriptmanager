332e9daf8b231bcda19a658474eea59451be5ac5
wkl2 <wkl2@psu.edu>
2015-02-18 10:33:48 -0500

    NEW FEATURE: extract fasta sequence from input bed file and genome file. automatically generates genome index if it does not exist. tests for unequal columns in genome fasta file.

---
+++ PughPipeline/src/scripts/FASTAExtract.java
@@ -0,0 +1,260 @@
+package scripts;
+
+import htsjdk.samtools.SAMException;
+import htsjdk.samtools.reference.IndexedFastaSequenceFile;
+
+import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import objects.BEDCoord;
+import util.LineReader;
+
+/*
+ * Adapted from:
+ * https://github.com/mdshw5/pyfaidx/blob/master/pyfaidx/__init__.py
+ * pyfaidx python program for manipulating fasta files efficiently
+ */
+
+@SuppressWarnings("serial")
+public class FASTAExtract extends JFrame {
+	private File GENOME = null;
+	private File OUTPUTPATH = null;
+	private ArrayList<File> BED = null;
+	private PrintStream OUT = null;
+	private boolean STRAND = true;
+	private boolean INDEX = true;
+	
+	private JTextArea textArea;
+	
+	public FASTAExtract(File gen, ArrayList<File> b, File out, boolean str) {
+		setTitle("BAM File Statistics");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(150, 150, 600, 800);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		
+		GENOME = gen;
+		BED = b;
+		OUTPUTPATH = out;
+		STRAND = str;
+	}
+	
+	public void run() throws IOException, InterruptedException {
+		File FAI = new File(GENOME + ".fai");
+		//Check if FAI index file exists
+		if(!FAI.exists() || FAI.isDirectory()) {
+			textArea.append("FASTA Index file not found.\nGenerating new one...\n");
+			INDEX = buildFASTAIndex(GENOME);
+		}
+		
+		if(INDEX) {
+			IndexedFastaSequenceFile QUERY = new IndexedFastaSequenceFile(GENOME);
+			for(int x = 0; x < BED.size(); x++) {
+				textArea.append("Proccessing File: " + BED.get(x).getName() + "\n");
+				//Open Output File
+				String NAME = BED.get(x).getName().split("\\.")[0] + ".fa";
+				if(OUTPUTPATH != null) {
+					try { OUT = new PrintStream(new File(OUTPUTPATH.getCanonicalPath() + File.separator + NAME)); }
+					catch (FileNotFoundException e) { e.printStackTrace(); }
+					catch (IOException e) {	e.printStackTrace(); }
+				} else {
+					try { OUT = new PrintStream(new File(NAME)); }
+					catch (FileNotFoundException e) { e.printStackTrace(); }
+				}
+				
+				ArrayList<BEDCoord> BED_Coord = loadCoord(BED.get(x));
+				
+				for(int y = 0; y < BED_Coord.size(); y++) {
+					try {
+						String seq = new String(QUERY.getSubsequenceAt(BED_Coord.get(y).getChrom(), BED_Coord.get(y).getStart() + 1, BED_Coord.get(y).getStop()).getBases());
+						if(STRAND && BED_Coord.get(y).getDir().equals("-")) {
+							seq = RevComplement(seq);
+						}
+						OUT.println(">" + BED_Coord.get(y).getName() + "\n" + seq);
+					} catch (SAMException e) {
+						textArea.append("INVALID COORDINATE: " + BED_Coord.get(y).toString() + "\n");
+					}
+				}
+				OUT.close();
+		        firePropertyChange("fa",x, x + 1);	
+			}
+			QUERY.close();
+			textArea.append("Extraction Complete\n");
+		} else {
+			textArea.append("Genome FASTA file contains invalid lines!!!\n");
+		}
+	}
+	
+	public String RevComplement (String SEQ) {
+		String RC = "";
+		for (int x = 0; x < SEQ.length(); x++){
+			if(SEQ.charAt(x) == 'A') { RC = 'T' + RC; }
+			if(SEQ.charAt(x) == 'T') { RC = 'A' + RC; }
+			if(SEQ.charAt(x) == 'G') { RC = 'C' + RC; }
+			if(SEQ.charAt(x) == 'C') { RC = 'G' + RC; }
+		}
+		return RC;
+	}
+	
+    public ArrayList<BEDCoord> loadCoord(File INPUT) throws FileNotFoundException {
+		Scanner scan = new Scanner(INPUT);
+		ArrayList<BEDCoord> COORD = new ArrayList<BEDCoord>();
+		while (scan.hasNextLine()) {
+			String[] temp = scan.nextLine().split("\t");
+			if(temp.length > 2) { 
+				if(!temp[0].contains("track") && !temp[0].contains("#")) {
+					String name = "";
+					if(temp.length > 3) { name = temp[3]; }
+					else { name = temp[0] + "_" + temp[1] + "_" + temp[2]; }
+					if(Integer.parseInt(temp[1]) >= 0) {
+						if(temp[5].equals("+")) { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+", name)); }
+						else { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "-", name)); }
+					} else {
+						System.out.println("Invalid Coordinate in File!!!\n" + Arrays.toString(temp));
+					}
+				}
+			}
+		}
+		scan.close();
+		return COORD;
+    }
+
+    //contig_name\tcontig_length\toffset_distance_from_last_contig\tcolumnlength\tcolumnlength_with_endline\n"
+    //chr1    230218  6       60      61
+    //chr2    813184  234067  60      61
+    public boolean buildFASTAIndex(File fasta) throws IOException {
+    	textArea.append(getTimeStamp() + "\nBuilding Genome Index...\n");
+    	
+    	boolean properFASTA = true;
+    	ArrayList<String> IMPROPER_FASTA = new ArrayList<String>();
+    	int counter = 0;
+
+    	String contig = "";
+    	int binaryOffset = 0;
+    	int currentOffset = 0;
+    	int contigLength = 0;
+    	int column_Length = 0;
+    	int untrimmed_Column_Length = 0;
+    	    	
+    	BufferedReader b_read = new BufferedReader(new FileReader(fasta));
+    	LineReader reader = new LineReader(b_read);
+    	PrintStream FAI = new PrintStream(fasta.getName() + ".fai");
+    	
+    	String strLine = "";
+    	while(!(strLine = reader.readLine()).equals("")) {
+    		//Pull parameters line
+    		int current_untrimmed_Column_Length = strLine.length();
+			int current_column_Length = strLine.trim().length();
+
+			if(strLine.contains(">")) {
+				if(IMPROPER_FASTA.size() > 1) {
+					textArea.append("Unequal column size FASTA Line at:\n");
+					for(int z = 0; z < IMPROPER_FASTA.size(); z++) {	textArea.append(contig + "\t" + IMPROPER_FASTA.get(z) + "\n");	}
+					properFASTA = false;
+					break;
+				}
+				if(counter > 0) { FAI.println(contig + "\t" + contigLength + "\t" + currentOffset + "\t" + column_Length + "\t" + untrimmed_Column_Length);	}
+				//Reset parameters for new contig
+				untrimmed_Column_Length = 0;
+				contigLength = 0;
+				column_Length = 0;
+				contig = strLine.trim().substring(1);
+				binaryOffset += current_untrimmed_Column_Length;
+				currentOffset = binaryOffset;
+				IMPROPER_FASTA = new ArrayList<String>();
+			} else {
+				if(untrimmed_Column_Length == 0) { untrimmed_Column_Length = current_untrimmed_Column_Length; }
+				if(column_Length == 0) { column_Length = current_column_Length;	}
+				binaryOffset += current_untrimmed_Column_Length;
+				contigLength += current_column_Length;
+				
+				//Check to make sure all the columns are equal. Index is invalid otherwise
+				if(current_untrimmed_Column_Length != untrimmed_Column_Length || current_untrimmed_Column_Length == 0) { IMPROPER_FASTA.add(strLine.trim());	}
+			}
+			counter++;
+    	}
+		FAI.println(contig + "\t" + contigLength + "\t" + currentOffset + "\t" + column_Length + "\t" + untrimmed_Column_Length);
+		b_read.close();
+    	FAI.close();
+    	
+		if(properFASTA) textArea.append("Genome Index Built\n" + getTimeStamp() + "\n");
+		else { new File(fasta.getName() + ".fai").delete(); }
+		
+		return properFASTA;
+    }
+    
+	private static String getTimeStamp() {
+		Date date= new Date();
+		String time = new Timestamp(date.getTime()).toString();
+		return time;
+	}
+}
+
+//def build_index(self):
+//
+//  def check_bad_lines(rname, bad_lines, i):
+//      if len(bad_lines) > 1:
+//          raise FastaIndexingError("Line length of fasta file is not consistent! Inconsistent line found in >{0} at line {1:n}.".format(rname, bad_lines[0] + 1))
+//      elif len(bad_lines) == 1:  # check that the line is previous line
+//          if bad_lines[0] + 1 != i:
+//              raise FastaIndexingError("Line length of fasta file is not consistent! Inconsistent line found in >{0} at line {1:n}.".format(rname, bad_lines[0] + 1))
+//
+//  with open(self.filename, 'r') as fastafile:
+//      with open(self.indexname, 'w') as indexfile:
+//          rname = None  # reference sequence name
+//          offset = 0  # binary offset of end of current line
+//          rlen = 0  # reference character length
+//          blen = None  # binary line length (includes newline)
+//          clen = None  # character line length
+//          bad_lines = []  # lines > || < than blen
+//          thisoffset = offset
+//          for i, line in enumerate(fastafile):
+//              line_blen = len(line)
+//              line_clen = len(line.rstrip('\n\r'))
+//              # write an index line
+//              if line[0] == '>':
+//                  check_bad_lines(rname, bad_lines, i)  # raises errors
+//                  if i > 0:
+//                      indexfile.write("{0}\t{1:d}\t{2:d}\t{3:d}\t{4:d}\n".format(rname, rlen, thisoffset, clen, blen))
+//                  blen = None
+//                  rlen = 0
+//                  clen = None
+//                  bad_lines = []
+//                  rname = line.rstrip()[1:].split()[0]
+//                  offset += line_blen
+//                  thisoffset = offset
+//              else:  # check line and advance offset
+//                  if not blen:
+//                      blen = line_blen
+//                  if not clen:
+//                      clen = line_clen
+//                  # only one short line should be allowed
+//                  # before we hit the next header, and it
+//                  # should be the last line in the entry
+//                  if line_blen != blen or line_blen == 0:
+//                      bad_lines.append(i)
+//                  offset += line_blen
+//                  rlen += line_clen
+//
+//          # write the final index line
+//          check_bad_lines(rname, bad_lines, i + 1)  # advance index since we're at the end of the file
+//          indexfile.write("{0}\t{1:d}\t{2:d}\t{3:d}\t{4:d}\n".format(rname, rlen, thisoffset, clen, blen))
---
+++ PughPipeline/src/window_interface/Data_Analysis/FASTAExtractWindow.java
@@ -0,0 +1,248 @@
+package window_interface.Data_Analysis;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.SpringLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.SwingWorker;
+import javax.swing.JProgressBar;
+import javax.swing.JLabel;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import scripts.FASTAExtract;
+import util.FileSelection;
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+
+
+@SuppressWarnings("serial")
+public class FASTAExtractWindow extends JFrame implements ActionListener, PropertyChangeListener {
+	private JPanel contentPane;
+	protected JFileChooser fc = new JFileChooser(new File(System.getProperty("user.dir")));	
+	
+	final DefaultListModel expList;
+	ArrayList<File> BEDFiles = new ArrayList<File>();
+	private File INPUT = null;
+	private File OUTPUT_PATH = null;
+	
+	private JButton btnLoad;
+	private JButton btnRemoveBam;
+	private JButton btnCalculate;
+	private JButton btnOutput;
+	private JLabel lblGenome;
+	private JLabel lblDefaultToLocal;
+	private JLabel lblCurrent;
+	private JProgressBar progressBar;
+	private JCheckBox chckbxStrand;
+
+	public Task task;
+	
+	class Task extends SwingWorker<Void, Void> {
+        @Override
+        public Void doInBackground() throws IOException {
+        	try {
+        		if(INPUT == null) {
+        			JOptionPane.showMessageDialog(null, "Genomic File Not Loaded!!!");
+        		} else if(BEDFiles.size() < 1) {
+        			JOptionPane.showMessageDialog(null, "No BAM Files Loaded!!!");
+        		} else {
+        			setProgress(0);
+        			FASTAExtract signal = new FASTAExtract(INPUT, BEDFiles, OUTPUT_PATH, chckbxStrand.isSelected());
+        				
+        			signal.addPropertyChangeListener("fa", new PropertyChangeListener() {
+					    public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+					    	int temp = (Integer) propertyChangeEvent.getNewValue();
+					    	int percentComplete = (int)(((double)(temp) / BEDFiles.size()) * 100);
+				        	setProgress(percentComplete);
+					     }
+					 });
+	        		
+        			signal.setVisible(true);
+        			signal.run();
+					
+        		}
+        	} catch(NumberFormatException nfe){
+				JOptionPane.showMessageDialog(null, "Invalid Input in Fields!!!");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        	return null;
+        }
+        
+        public void done() {
+        	massXable(contentPane, true);
+            setCursor(null); //turn off the wait cursor
+        }
+	}
+	
+	public FASTAExtractWindow() {
+		setTitle("FASTA Extract from BED");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		setBounds(125, 125, 450, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
+	
+		JScrollPane scrollPane = new JScrollPane();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane, 97, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane, -10, SpringLayout.EAST, contentPane);
+		contentPane.add(scrollPane);
+		
+      	expList = new DefaultListModel();
+		final JList listExp = new JList(expList);
+		listExp.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		scrollPane.setViewportView(listExp);
+		
+		btnLoad = new JButton("Load BED Files");
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnLoad, 0, SpringLayout.WEST, scrollPane);
+		btnLoad.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				File[] newBEDFiles = FileSelection.getBEDFiles(fc);
+				if(newBEDFiles != null) {
+					for(int x = 0; x < newBEDFiles.length; x++) { 
+						BEDFiles.add(newBEDFiles[x]);
+						expList.addElement(newBEDFiles[x].getName());
+					}
+				}
+			}
+		});
+		contentPane.add(btnLoad);
+		
+		btnRemoveBam = new JButton("Remove BED");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnLoad, 0, SpringLayout.NORTH, btnRemoveBam);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnRemoveBam, -11, SpringLayout.NORTH, scrollPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnRemoveBam, -10, SpringLayout.EAST, contentPane);
+		btnRemoveBam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				while(listExp.getSelectedIndex() > -1) {
+					BEDFiles.remove(listExp.getSelectedIndex());
+					expList.remove(listExp.getSelectedIndex());
+				}
+			}
+		});		
+		contentPane.add(btnRemoveBam);
+		
+		btnCalculate = new JButton("Calculate");
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane, -71, SpringLayout.NORTH, btnCalculate);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnCalculate, 165, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnCalculate, -165, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnCalculate, -10, SpringLayout.SOUTH, contentPane);
+		contentPane.add(btnCalculate);
+		
+		progressBar = new JProgressBar();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, progressBar, 3, SpringLayout.NORTH, btnCalculate);
+		sl_contentPane.putConstraint(SpringLayout.EAST, progressBar, -10, SpringLayout.EAST, contentPane);
+        progressBar.setStringPainted(true);
+		contentPane.add(progressBar);
+		
+        btnCalculate.setActionCommand("start");
+        
+        JButton btnLoadGenome = new JButton("Load Genome FASTA");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, btnLoadGenome, 0, SpringLayout.NORTH, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.WEST, btnLoadGenome, 0, SpringLayout.WEST, scrollPane);
+        contentPane.add(btnLoadGenome);
+        
+        lblGenome = new JLabel("No Genomic FASTA File Loaded");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, lblGenome, 10, SpringLayout.SOUTH, btnLoadGenome);
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblGenome, 0, SpringLayout.WEST, btnLoad);
+        sl_contentPane.putConstraint(SpringLayout.EAST, lblGenome, 0, SpringLayout.EAST, contentPane);
+        contentPane.add(lblGenome);
+        
+        lblCurrent = new JLabel("Current Output:");
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblCurrent, 0, SpringLayout.WEST, scrollPane);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, lblCurrent, -45, SpringLayout.SOUTH, contentPane);
+        lblCurrent.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+        contentPane.add(lblCurrent);
+        
+        lblDefaultToLocal = new JLabel("Default to Local Directory");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, lblDefaultToLocal, 1, SpringLayout.NORTH, lblCurrent);
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblDefaultToLocal, 6, SpringLayout.EAST, lblCurrent);
+        lblDefaultToLocal.setBackground(Color.WHITE);
+        contentPane.add(lblDefaultToLocal);
+        
+        btnOutput = new JButton("Output Directory");
+        sl_contentPane.putConstraint(SpringLayout.WEST, btnOutput, 150, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.EAST, btnOutput, -150, SpringLayout.EAST, contentPane);
+        btnOutput.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		OUTPUT_PATH = FileSelection.getOutputDir(fc);
+    			if(OUTPUT_PATH != null) {
+    				lblDefaultToLocal.setText(OUTPUT_PATH.getAbsolutePath());
+    			}
+        	}
+        });
+        sl_contentPane.putConstraint(SpringLayout.NORTH, btnOutput, 6, SpringLayout.SOUTH, scrollPane);
+        contentPane.add(btnOutput);
+        
+        chckbxStrand = new JCheckBox("Force Strandedness");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, chckbxStrand, 1, SpringLayout.NORTH, btnLoad);
+        sl_contentPane.putConstraint(SpringLayout.EAST, chckbxStrand, -17, SpringLayout.WEST, btnRemoveBam);
+        chckbxStrand.setSelected(true);
+        contentPane.add(chckbxStrand);
+
+        btnLoadGenome.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		File temp = FileSelection.getFASTAFile(fc);
+				if(temp != null) {
+					INPUT = temp;
+					lblGenome.setText(INPUT.getName());
+				}
+        	}
+        });
+        btnCalculate.addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		massXable(contentPane, false);
+    	setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        
+        task = new Task();
+        task.addPropertyChangeListener(this);
+        task.execute();
+	}
+	
+	/**
+     * Invoked when task's progress property changes.
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress" == evt.getPropertyName()) {
+            int progress = (Integer) evt.getNewValue();
+            progressBar.setValue(progress);
+        }
+    }
+	
+	public void massXable(Container con, boolean status) {
+		for(Component c : con.getComponents()) {
+			c.setEnabled(status);
+			if(c instanceof Container) { massXable((Container)c, status); }
+		}
+	}
+}
+
+
+	
