4ff4f7f35465ef9324ea7e877da801b90c723954
wkl2 <wkl2@psu.edu>
2015-02-19 08:56:26 -0500

    added support for "fsa" fasta files and "gff3" gff files

--- PughPipeline/src/util/ExtensionFileFilter.java
+++ PughPipeline/src/util/ExtensionFileFilter.java
@@ -9,18 +9,19 @@ public class ExtensionFileFilter extends FileFilter{
 		
 	private String ext = "";
 	private String ext2 = "";
+	private String ext3 = "";
 	
 	public ExtensionFileFilter(String filter) {
 		ext = filter;
-		if(ext.equals("fa")) { ext2 = "fasta"; }
-		if(ext.equals("gff")) { ext2 = "gtf"; }
+		if(ext.equals("fa")) { ext2 = "fasta"; ext3 = "fsa"; }
+		if(ext.equals("gff")) { ext2 = "gtf"; ext3 = "gff3"; }
 	}
 	
 	public boolean accept(File f) {
 		if (f.isDirectory()) return true;
 		String extension = getExtension(f);
 		if (extension != null) {
-			if (extension.equals(ext) || extension.equals(ext2)) {
+			if (extension.equals(ext) || extension.equals(ext2) || extension.equals(ext3)) {
 				return true;
 			} else {
 				return false;
