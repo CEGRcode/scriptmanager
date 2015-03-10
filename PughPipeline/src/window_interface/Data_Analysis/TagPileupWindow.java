1de79b2119b03986db60edb23d387aba4a0f6a8f
wkl2 <wkl2@psu.edu>
2015-03-10 14:29:07 -0400

    fix bug where BED coordinate without +/- crashes program. now assigns "+" by default

--- PughPipeline/src/window_interface/Data_Analysis/TagPileupWindow.java
+++ PughPipeline/src/window_interface/Data_Analysis/TagPileupWindow.java
@@ -706,8 +706,11 @@ public class TagPileupWindow extends JFrame implements ActionListener, PropertyC
 					if(temp.length > 3) { name = temp[3]; }
 					else { name = temp[0] + "_" + temp[1] + "_" + temp[2]; }
 					if(Integer.parseInt(temp[1]) >= 0) {
-						if(temp[5].equals("+")) { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+", name)); }
-						else { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "-", name)); }
+						if(temp.length > 4) { 
+							if(temp[5].equals("+")) { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+", name)); }
+							else { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "-", name)); }
+						} else { COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+", name)); }
+
 					} else {
 						System.out.println("Invalid Coordinate in File!!!\n" + Arrays.toString(temp));
 					}
