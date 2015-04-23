7e986fd17d840738f75160f6c43c01c047a50696
wkl2 <wkl2@psu.edu>
2015-04-23 10:58:23 -0400

    Heatplot upgrade. Heatmap options button can now redraw the heatmap on the fly given user-inputs

--- PughPipeline/src/scripts/Visualization/HeatMapPlot.java
+++ PughPipeline/src/scripts/Visualization/HeatMapPlot.java
@@ -1,191 +1,54 @@
 package scripts.Visualization;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Vector;
 
-import javax.swing.ImageIcon;
-import javax.swing.JColorChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JSplitPane;
+import javax.swing.JLayeredPane;
+import javax.swing.JTabbedPane;
+import javax.swing.SpringLayout;
 
-import org.jfree.chart.ChartPanel;
-
-import charts.CompositePlot;
-import util.CDTUtilities;
-import window_interface.Visualization.HeatMapOptions;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import scripts.Visualization.HeatmapScripts.HeatMapPanel;
 
 @SuppressWarnings("serial")
 public class HeatMapPlot extends JFrame {
 	
-	private File INPUT = null;
+	private Vector<File> INPUT = null;
+	@SuppressWarnings("unused")
+	private boolean MATCH_FILES = false;
 	public File OUTPUT = null;
-		
-	private HeatMapOptions option = null;
-	public Color MINCOLOR = null;
-	public Color MAXCOLOR = null;
-
-	public ArrayList<Double> STATS = null;
-	private Vector<double[]> CDT = null;
-	public double COLOR_RATIO = 0;
-	public boolean allowResize = true;
-		
-	private ChartPanel COMPOSITE_PLOT = null;
-	public BufferedImage HEAT_PLOT = null;
 	
-	private JPanel backPanel;
-	private JPanel pnlOptions;
-	public JLabel heatLabel;
-	private JScrollPane heatPane;
-	private JSplitPane HsplitPane;
-	private JSplitPane VsplitPane;
-	private JButton btnCompositeSeriesColor;
+	final JLayeredPane layeredPane;
+	final JTabbedPane tabbedPane;
 	 
-	public HeatMapPlot(File in, Color mi, Color ma, File out) {
-		setTitle("Heatmap");
+	public HeatMapPlot(Vector<File> in, boolean match) {
+		setTitle("CDT File Visualization");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(150, 150, 1000, 600);
 		getContentPane().setLayout(new BorderLayout());
 		
-		backPanel = new JPanel();
-		getContentPane().add(backPanel);
-		backPanel.setLayout(new BorderLayout());
-		heatLabel = new JLabel();
-		pnlOptions = new JPanel();
+		layeredPane = new JLayeredPane();
+		getContentPane().add(layeredPane, BorderLayout.CENTER);
+		SpringLayout sl_layeredPane = new SpringLayout();
+		layeredPane.setLayout(sl_layeredPane);
 		
-		btnCompositeSeriesColor = new JButton("Composite Options");
-		btnCompositeSeriesColor.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				COMPOSITE_PLOT.getChart().getXYPlot().getRenderer().setSeriesPaint(0, JColorChooser.showDialog(btnCompositeSeriesColor, "Select Composite Series Color", Color.WHITE));
-			}
-		});
-		pnlOptions.add(btnCompositeSeriesColor);
-		JButton btnOptions = new JButton("HeatMap Options");
-		btnOptions.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-					option.setVisible(true);
-			}
-		});
-		pnlOptions.add(btnOptions);
-		
-		HsplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-		HsplitPane.setResizeWeight(0.5);
-		HsplitPane.setOneTouchExpandable(true);
-		HsplitPane.setDividerLocation(0.5);
-		
-		VsplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-		VsplitPane.setResizeWeight(1.0);
-		VsplitPane.setLeftComponent(HsplitPane);
-		VsplitPane.setRightComponent(pnlOptions);
-		VsplitPane.setOneTouchExpandable(true);
-		backPanel.add(VsplitPane);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		sl_layeredPane.putConstraint(SpringLayout.NORTH, tabbedPane, 6, SpringLayout.NORTH, layeredPane);
+		sl_layeredPane.putConstraint(SpringLayout.WEST, tabbedPane, 6, SpringLayout.WEST, layeredPane);
+		sl_layeredPane.putConstraint(SpringLayout.SOUTH, tabbedPane, -6, SpringLayout.SOUTH, layeredPane);
+		sl_layeredPane.putConstraint(SpringLayout.EAST, tabbedPane, -6, SpringLayout.EAST, layeredPane);
+		layeredPane.add(tabbedPane);
 		
 		INPUT = in;
-		OUTPUT = out;
-		MINCOLOR = mi;
-		MAXCOLOR = ma;
+		MATCH_FILES = match;
 	}
 	
 	public void run() throws IOException {
-		//Load CDT File into memory && calculate basics stats
-		CDT = CDTUtilities.loadCDT(INPUT);
-		STATS = CDTUtilities.getStats(CDT);
-		
-		//Generate composite plot
-		double[] COMPOSITE = CDTUtilities.getComposite(CDT);
-		double[] DOMAIN = new double[COMPOSITE.length];
-		ArrayList<Color> COLOR = new ArrayList<Color>();
-		COLOR.add(MAXCOLOR);
-		for(int x = 0; x < DOMAIN.length; x++) { DOMAIN[x] = x; }
-		COMPOSITE_PLOT = (ChartPanel) CompositePlot.createCompositePlot(DOMAIN, COMPOSITE, INPUT.getName(), COLOR);
-		
-		
-		//Generate heatmap
-		COLOR_RATIO = 2 * STATS.get(3); //default max color is 2 * mean
-		HEAT_PLOT = generateHeatMap(CDT, COLOR_RATIO);	//Keep copy of original in memory
-		BufferedImage heatmap = resize(HEAT_PLOT, 300, 500);
-		
-		//push both into heatmap
-		HsplitPane.setLeftComponent(COMPOSITE_PLOT);
-		heatLabel.setIcon(new ImageIcon(heatmap));
-		heatPane = new JScrollPane(heatLabel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		HsplitPane.setRightComponent(heatPane);
-
-		//detect window resize and resize heatmap if appropriate
-		heatPane.addComponentListener(new java.awt.event.ComponentAdapter() {
-            public void componentResized(java.awt.event.ComponentEvent evt) {
-            	if(allowResize) { resizeLabel(); }
-			}
-        });
-		
-		option = new HeatMapOptions(this);
-		option.populate();
+		for(int x = 0; x < INPUT.size(); x++) {
+			HeatMapPanel newpanel = new HeatMapPanel(INPUT.get(x));
+			tabbedPane.add(INPUT.get(x).getName(), newpanel.generatePanel());
+		}
 	}
-	
-	public BufferedImage generateHeatMap(Vector<double[]> CDT, double SCALE) throws FileNotFoundException {
-		int width = 2;
-		int height = 2;
-		
-		int pixwidth = CDT.get(0).length * width;
-		int pixheight = CDT.size() * height;
-		
-		System.setProperty("java.awt.headless", "true");
-		BufferedImage im = new BufferedImage(pixwidth, pixheight, BufferedImage.TYPE_INT_ARGB);
-        Graphics g = im.getGraphics();
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.WHITE);
-        g2.fillRect(0,0,pixwidth, pixheight);
-        
-        int count = 0;
-        for (int x = 0; x < CDT.size(); x++){
-        	double[] ID = CDT.get(x);
-
-        	for (int j=0;j< ID.length;j++){
-        		if(ID[j] > 0) {
-        			double v = ID[j] / SCALE;
-        			double sVal = v>1 ? 1 : (v<0 ? 0 : v);
-            		int red = (int)(MAXCOLOR.getRed() * sVal + MINCOLOR.getRed() * (1 - sVal));
-            	    int green = (int)(MAXCOLOR.getGreen() * sVal + MINCOLOR.getGreen() * (1 - sVal));
-            	    int blue = (int)(MAXCOLOR.getBlue() *sVal + MINCOLOR.getBlue() * (1 - sVal));        			
-        			
-        			g.setColor(new Color(red, green, blue));
-        		}
-        		else g.setColor(Color.WHITE);
-                g.fillRect(j*width, count*height, width, height);
-        	}
-            count++;
-        }
-        
-        return im;
-	}
-
-	public void resizeLabel() {
-	    heatLabel.setIcon(new ImageIcon(HEAT_PLOT.getScaledInstance(HsplitPane.getRightComponent().getWidth(), HsplitPane.getRightComponent().getHeight(), Image.SCALE_FAST)));
-	}
-	
-	public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
-	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
-	    Graphics2D g2d = dimg.createGraphics();
-	    g2d.drawImage(tmp, 0, 0, null);
-	    g2d.dispose();
-
-	    return dimg;
-	}  
 }
--- PughPipeline/src/window_interface/Visualization/HeatMapWindow.java
+++ PughPipeline/src/window_interface/Visualization/HeatMapWindow.java
@@ -10,17 +10,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JColorChooser;
 import javax.swing.ListSelectionModel;
 import javax.swing.SpringLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
 import javax.swing.SwingWorker;
 import javax.swing.JProgressBar;
-import javax.swing.JLabel;
 
-import java.awt.Font;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Cursor;
@@ -31,6 +27,7 @@ import java.beans.PropertyChangeListener;
 
 import scripts.Visualization.HeatMapPlot;
 import util.FileSelection;
+import javax.swing.JCheckBox;
 
 @SuppressWarnings("serial")
 public class HeatMapWindow extends JFrame implements ActionListener, PropertyChangeListener {
@@ -39,40 +36,24 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 	
 	final DefaultListModel expList;
 	Vector<File> cdtFiles = new Vector<File>();
-	private File OUTPUTPATH = null;
-	private Color MIN_COLOR = Color.WHITE;
-	private Color MAX_COLOR = Color.RED;
 	
 	private JButton btnLoad;
 	private JButton btnRemoveBam;
 	private JButton btnGen;
-	private JButton btnOutputDirectory;
-	private JButton btnChooseMinColor;
-	private JLabel lblCurrentOutput;
-	private JLabel lblDefaultToLocal;
-	private JLabel lblMinColor;
+	private JCheckBox chckbxAutomaticallyMatchSenseantisense;
 	private JProgressBar progressBar;
 		
 	public Task task;
-	private JLabel lblMaxColor;
 	
 	class Task extends SwingWorker<Void, Void> {
         @Override
         public Void doInBackground() throws IOException {
         	setProgress(0);
-        	if(OUTPUTPATH == null) {
-        		OUTPUTPATH = new File(System.getProperty("user.dir"));
-        	}
-        	for(int x = 0; x < cdtFiles.size(); x++) {
-        		String[] out = cdtFiles.get(x).getName().split("\\.");
+        	
+        	HeatMapPlot heat = new HeatMapPlot(cdtFiles, chckbxAutomaticallyMatchSenseantisense.isSelected());
+        	heat.setVisible(true);
+        	heat.run();
 
-        		HeatMapPlot heat = new HeatMapPlot(cdtFiles.get(x), MIN_COLOR, MAX_COLOR, new File(OUTPUTPATH + File.separator + out[0] + ".png"));
-        		heat.setVisible(true);
-				heat.run();
-        		
-				int percentComplete = (int)(((double)(x + 1) / cdtFiles.size()) * 100);
-        		setProgress(percentComplete);
-        	}
         	setProgress(100);
         	return null;
         }
@@ -87,7 +68,7 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 		setTitle("Heat Map Plot Generator");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		setBounds(125, 125, 450, 350);
+		setBounds(125, 125, 450, 315);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -95,8 +76,9 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 		contentPane.setLayout(sl_contentPane);
 	
 		JScrollPane scrollPane = new JScrollPane();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane, 35, SpringLayout.NORTH, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane, 0, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane, -10, SpringLayout.EAST, contentPane);
 		contentPane.add(scrollPane);
 		
       	expList = new DefaultListModel();
@@ -105,8 +87,8 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 		scrollPane.setViewportView(listExp);
 		
 		btnLoad = new JButton("Load CDT Files");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane, 11, SpringLayout.SOUTH, btnLoad);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnLoad, 5, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnLoad, 10, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnLoad, 0, SpringLayout.NORTH, contentPane);
 		btnLoad.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 				File[] newCDTFiles = FileSelection.getCDTFiles(fc);
@@ -121,9 +103,8 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 		contentPane.add(btnLoad);
 		
 		btnRemoveBam = new JButton("Remove CDT Files");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnLoad, 0, SpringLayout.NORTH, btnRemoveBam);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnRemoveBam, 0, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnRemoveBam, -5, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnRemoveBam, 0, SpringLayout.NORTH, btnLoad);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnRemoveBam, -10, SpringLayout.EAST, contentPane);
 		btnRemoveBam.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				while(listExp.getSelectedIndex() > -1) {
@@ -135,7 +116,6 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 		contentPane.add(btnRemoveBam);
 		
 		btnGen = new JButton("Generate");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane, -126, SpringLayout.NORTH, btnGen);
 		sl_contentPane.putConstraint(SpringLayout.WEST, btnGen, 167, SpringLayout.WEST, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnGen, 0, SpringLayout.SOUTH, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.EAST, btnGen, -175, SpringLayout.EAST, contentPane);
@@ -149,80 +129,12 @@ public class HeatMapWindow extends JFrame implements ActionListener, PropertyCha
 		
         btnGen.setActionCommand("start");
         
-        lblCurrentOutput = new JLabel("Current Output:");
-        sl_contentPane.putConstraint(SpringLayout.WEST, lblCurrentOutput, 5, SpringLayout.WEST, contentPane);
-        lblCurrentOutput.setFont(new Font("Lucida Grande", Font.BOLD, 13));
-        contentPane.add(lblCurrentOutput);
-        
-        lblDefaultToLocal = new JLabel("Default to Local Directory");
-        sl_contentPane.putConstraint(SpringLayout.NORTH, lblDefaultToLocal, 10, SpringLayout.SOUTH, lblCurrentOutput);
-        sl_contentPane.putConstraint(SpringLayout.WEST, lblDefaultToLocal, 10, SpringLayout.WEST, lblCurrentOutput);
-        sl_contentPane.putConstraint(SpringLayout.EAST, lblDefaultToLocal, 313, SpringLayout.EAST, lblCurrentOutput);
-        lblDefaultToLocal.setFont(new Font("Dialog", Font.PLAIN, 12));
-        lblDefaultToLocal.setBackground(Color.WHITE);
-        contentPane.add(lblDefaultToLocal);
-        
-        btnOutputDirectory = new JButton("Output Directory");
-        sl_contentPane.putConstraint(SpringLayout.NORTH, btnOutputDirectory, 40, SpringLayout.SOUTH, scrollPane);
-        sl_contentPane.putConstraint(SpringLayout.WEST, btnOutputDirectory, 145, SpringLayout.WEST, contentPane);
-        sl_contentPane.putConstraint(SpringLayout.EAST, btnOutputDirectory, -145, SpringLayout.EAST, contentPane);
-        contentPane.add(btnOutputDirectory);
-        
-        btnChooseMinColor = new JButton("Choose Min Color");
-        btnChooseMinColor.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		Color temp = JColorChooser.showDialog(btnChooseMinColor, "Select a Background Color", MIN_COLOR);
-        		if(temp != null) {
-        			MIN_COLOR = temp;
-        			lblMinColor.setBackground(MIN_COLOR);
-        		}
-        	}
-        });
-        sl_contentPane.putConstraint(SpringLayout.NORTH, lblCurrentOutput, 42, SpringLayout.SOUTH, btnChooseMinColor);
-        sl_contentPane.putConstraint(SpringLayout.NORTH, btnChooseMinColor, 6, SpringLayout.SOUTH, scrollPane);
-        sl_contentPane.putConstraint(SpringLayout.WEST, btnChooseMinColor, 0, SpringLayout.WEST, scrollPane);
-        contentPane.add(btnChooseMinColor);
-        
-        lblMinColor = new JLabel("");
-        sl_contentPane.putConstraint(SpringLayout.NORTH, lblMinColor, 6, SpringLayout.SOUTH, scrollPane);
-        sl_contentPane.putConstraint(SpringLayout.WEST, lblMinColor, 0, SpringLayout.WEST, btnGen);
-        sl_contentPane.putConstraint(SpringLayout.SOUTH, lblMinColor, -9, SpringLayout.NORTH, btnOutputDirectory);
-        lblMinColor.setOpaque(true);
-        lblMinColor.setBackground(MIN_COLOR);
-        contentPane.add(lblMinColor);
-        
-        JButton btnChooseMaxColor = new JButton("Choose Max Color");
-        btnChooseMaxColor.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		Color temp = JColorChooser.showDialog(btnChooseMinColor, "Select a Background Color", MAX_COLOR);
-        		if(temp != null) {
-        			MAX_COLOR = temp;
-        			lblMaxColor.setBackground(MAX_COLOR);
-        		}
-        	}
-        });
-        sl_contentPane.putConstraint(SpringLayout.EAST, lblMinColor, -20, SpringLayout.WEST, btnChooseMaxColor);
-        sl_contentPane.putConstraint(SpringLayout.WEST, btnChooseMaxColor, 212, SpringLayout.WEST, contentPane);
-        sl_contentPane.putConstraint(SpringLayout.NORTH, btnChooseMaxColor, 6, SpringLayout.SOUTH, scrollPane);
-        contentPane.add(btnChooseMaxColor);
-        
-        lblMaxColor = new JLabel("");
-        sl_contentPane.putConstraint(SpringLayout.NORTH, lblMaxColor, 6, SpringLayout.SOUTH, scrollPane);
-        sl_contentPane.putConstraint(SpringLayout.WEST, lblMaxColor, 15, SpringLayout.EAST, btnChooseMaxColor);
-        sl_contentPane.putConstraint(SpringLayout.SOUTH, lblMaxColor, 0, SpringLayout.SOUTH, btnChooseMaxColor);
-        sl_contentPane.putConstraint(SpringLayout.EAST, lblMaxColor, 40, SpringLayout.EAST, btnChooseMaxColor);
-        lblMaxColor.setOpaque(true);
-        lblMaxColor.setBackground(MAX_COLOR);
-        contentPane.add(lblMaxColor);
-        
-        btnOutputDirectory.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	OUTPUTPATH = FileSelection.getOutputDir(fc);
-				if(OUTPUTPATH != null) {
-					lblDefaultToLocal.setText(OUTPUTPATH.getAbsolutePath());
-				}
-			}
-		});
+        chckbxAutomaticallyMatchSenseantisense = new JCheckBox("Automatically Match Sense-Antisense");
+        sl_contentPane.putConstraint(SpringLayout.WEST, chckbxAutomaticallyMatchSenseantisense, 85, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.EAST, chckbxAutomaticallyMatchSenseantisense, -85, SpringLayout.EAST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane, -6, SpringLayout.NORTH, chckbxAutomaticallyMatchSenseantisense);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, chckbxAutomaticallyMatchSenseantisense, -6, SpringLayout.NORTH, btnGen);
+        contentPane.add(chckbxAutomaticallyMatchSenseantisense);
         
         btnGen.addActionListener(this);
 	}
