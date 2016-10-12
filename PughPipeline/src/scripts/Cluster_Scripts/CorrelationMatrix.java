6c41c692994eacc3057d32c0cc66fcbedfd0a15e
William Lai <wkl2@psu.edu>
2016-10-12 16:54:53 -0400

    NEW FEATURE: first working draft of correlation matrix calculator. requires both row and column headers.

--- PughPipeline/src/scripts/Cluster_Scripts/CorrelationMatrix.java
+++ PughPipeline/src/scripts/Cluster_Scripts/CorrelationMatrix.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import util.SimilarityMetric;
 
@@ -12,48 +13,126 @@ public class CorrelationMatrix {
 	
 	private File INPUT = null;
 	private File OUT_PATH = null;
-	private PrintStream OUTPUT = null;
 	private int METRIC = 0;	
+	private boolean COLUMN = true;
 	
-	public CorrelationMatrix(File in, File out_path, int m) {
+	public CorrelationMatrix(File in, File out_path, int m, boolean col) {
 		INPUT = in;
 		OUT_PATH = out_path;
 		METRIC = m;
+		COLUMN = col;
 	}
 	
-	public void run() {
-		String[] name =INPUT.getName().split("\\.");
+	public void run() throws IOException {
+		ArrayList<double[]> DATA = new ArrayList<double[]>();
+		ArrayList<String> ID = new ArrayList<String>();	
+		
+		Scanner scan = new Scanner(INPUT);
+		int counter = 0;
+		boolean LOADFAIL = false;
+		int columnsize = 0;		
+		while (scan.hasNextLine()) {
+			String[] temp = scan.nextLine().split("\t");
+			if(counter == 0) {
+				columnsize = temp.length;
+				if(COLUMN) { for(int x = 1; x < temp.length; x++) { ID.add(temp[x]); } }
+			} else {
+				if(columnsize != temp.length) {
+					LOADFAIL = true;
+					scan.close();
+				} 
+				if(!COLUMN) { ID.add(temp[0]); }
+				double[] score = new double[columnsize - 1];
+				for(int x = 1; x < columnsize; x++) {
+					if(temp[x].isEmpty()) {
+						LOADFAIL = true;
+						scan.close();
+					} else {
+						score[x - 1] = Double.parseDouble(temp[x]);
+					}
+				}
+				DATA.add(score);
+			}			
+			counter++;
+	    }
+		scan.close();
+						
+		if(COLUMN) {
+			ArrayList<double[]> newDATA = new ArrayList<double[]>();
+			for(int x = 0; x < ID.size(); x++) {
+				double[] array = new double[DATA.size()];
+				for(int y = 0; y < DATA.size(); y++) {
+					array[y] = DATA.get(y)[x];
+				}
+				newDATA.add(array);
+			}
+			DATA = newDATA;
+		}
+		
+		if(LOADFAIL) {
+			outputMatrixLoadFail();
+			System.out.println(INPUT.getName() + "\tLoad failed due to non-numeric values in matrix");
+		} else {
+			double[][] MATRIX = calculateMatrix(DATA);
+			outputMatrix(MATRIX, ID);
+		}
+	}
+	
+	public void outputMatrixLoadFail() throws FileNotFoundException, IOException {
+		String[] name = INPUT.getName().split("\\.");
 		String NEWNAME = "";
 		for(int x = 0; x < name.length - 1; x++) {
-			if(x == name.length - 2) { NEWNAME += (name[x] + "_RAND.fa"); }
+			if(x == name.length - 2) { NEWNAME += (name[x]); }
 			else { NEWNAME += (name[x] + "."); }
 		}
 		
 		//Open Output File
-		if(OUT_PATH != null) {
-			try {
-				OUTPUT = new PrintStream(new File(OUT_PATH.getCanonicalPath() + File.separator + NEWNAME + "_MATRIX.out"));
-			} catch (FileNotFoundException e) { e.printStackTrace(); }
-			catch (IOException e) {	e.printStackTrace(); }
-		}
-		
+		PrintStream OUT = null;
+		if(OUT_PATH != null) { OUT = new PrintStream(new File(OUT_PATH.getCanonicalPath() + File.separator + NEWNAME + "_CORRMATRIX.out"));
+		} else { OUT = new PrintStream(new File(NEWNAME + "_MATRIX.out")); }
+		OUT.println(INPUT.getName() + "\tLoad failed due to non-numeric values in matrix");
+		OUT.close();
+	}
+	
+	public double[][] calculateMatrix(ArrayList<double[]> data) {
 		SimilarityMetric corr = new SimilarityMetric();
 		if(METRIC == 0) { corr.setType("pearson"); }
 		else if(METRIC == 1) { corr.setType("reflective"); }
 		else if(METRIC == 2) { corr.setType("spearman"); }
 		else if(METRIC == 3) { corr.setType("euclidean"); }
 		else if(METRIC == 4) { corr.setType("manhattan"); }
-
-		ArrayList<double[]> MATRIX = loadMatrix(INPUT);
 		
-		
-		
-		OUTPUT.close();
+		double[][] matrix = new double[data.size()][data.size()];
+		for(int x = 0; x < data.size(); x++) {
+			for(int y = x; y < data.size(); y++) {
+				double score = corr.getScore(data.get(x), data.get(y));
+				matrix[x][y] = score;
+				matrix[y][x] = score;
+			}
+		}
+		return matrix;
 	}
 	
-	public ArrayList<double[]> loadMatrix(File in) {
-		ArrayList<double[]> matrix = new ArrayList<double[]>();
+	public void outputMatrix(double[][] matrix, ArrayList<String> id) throws IOException {
+		String[] name = INPUT.getName().split("\\.");
+		String NEWNAME = "";
+		for(int x = 0; x < name.length - 1; x++) {
+			if(x == name.length - 2) { NEWNAME += (name[x]); }
+			else { NEWNAME += (name[x] + "."); }
+		}
+			
+		//Open Output File
+		PrintStream OUT = null;
+		if(OUT_PATH != null) { OUT = new PrintStream(new File(OUT_PATH.getCanonicalPath() + File.separator + NEWNAME + "_CORRMATRIX.out"));
+		} else { OUT = new PrintStream(new File(NEWNAME + "_MATRIX.out")); }
 		
-		return matrix;
+		for(int x = 0; x < id.size(); x++) { OUT.print("\t" + id.get(x)); }
+		OUT.println();
+		for(int x = 0; x < matrix.length; x++) {
+			OUT.print(id.get(x));
+			for(int y = 0; y < matrix.length; y++) { OUT.print("\t" + matrix[x][y]); }
+			OUT.println();
+		}
+		OUT.close();
 	}
 }
---
+++ PughPipeline/src/window_interface/Cluster_Tools/CorrelationMatrixWindow.java
@@ -0,0 +1,236 @@
+package window_interface.Cluster_Tools;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.SpringLayout;
+import javax.swing.SwingWorker;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+
+import scripts.Cluster_Scripts.CorrelationMatrix;
+import util.FileSelection;
+
+@SuppressWarnings("serial")
+public class CorrelationMatrixWindow extends JFrame implements ActionListener, PropertyChangeListener {
+	private JPanel contentPane;
+	protected JFileChooser fc = new JFileChooser(new File(System.getProperty("user.dir")));	
+	
+	final DefaultListModel<String> expList;
+	ArrayList<File> TABFiles = new ArrayList<File>();
+	private File OUTPUT_PATH = null;
+	
+	private JButton btnLoad;
+	private JButton btnRemoveBam;
+	private JButton btnCalculate;
+	private JButton btnOutput;
+	private JLabel lblDefaultToLocal;
+	private JLabel lblCurrent;
+	private JProgressBar progressBar;
+
+	private JRadioButton rdbtnCorrelateColumns;
+	private JRadioButton rdbtnCorrelateRows;
+	private JComboBox<String> comboBox;
+	
+	public Task task;
+	
+	class Task extends SwingWorker<Void, Void> {
+        @Override
+        public Void doInBackground() throws IOException {
+        	if(TABFiles.size() < 1) {
+        		JOptionPane.showMessageDialog(null, "No TAB Files Loaded!!!");
+        	} else {
+        		setProgress(0);
+        		
+        		for(int x = 0; x < TABFiles.size(); x++) {
+        			CorrelationMatrix matrix = new CorrelationMatrix(TABFiles.get(x), OUTPUT_PATH, comboBox.getSelectedIndex(), rdbtnCorrelateColumns.isSelected());
+            		matrix.run();
+
+            		int percentComplete = (int)(((double)(x + 1) / TABFiles.size()) * 100);
+   			       	setProgress(percentComplete);
+           		}
+           		setProgress(100);
+        		JOptionPane.showMessageDialog(null, "All Matrices Generated");	
+       		}
+        	return null;
+        }
+        
+        public void done() {
+        	massXable(contentPane, true);
+            setCursor(null); //turn off the wait cursor
+        }
+	}
+	
+	public CorrelationMatrixWindow() {
+		setTitle("Generate Correlation Matrix");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		setBounds(125, 125, 450, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
+	
+		JScrollPane scrollPane = new JScrollPane();
+		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane, -10, SpringLayout.EAST, contentPane);
+		contentPane.add(scrollPane);
+		
+      	expList = new DefaultListModel<String>();
+		final JList<String> listExp = new JList<String>(expList);
+		listExp.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		scrollPane.setViewportView(listExp);
+		
+		btnLoad = new JButton("Load TAB Files");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnLoad, 6, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane, 10, SpringLayout.SOUTH, btnLoad);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnLoad, 10, SpringLayout.WEST, contentPane);
+		btnLoad.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				File[] newTABFiles = FileSelection.getGenericFiles(fc);
+				if(newTABFiles != null) {
+					for(int x = 0; x < newTABFiles.length; x++) { 
+						TABFiles.add(newTABFiles[x]);
+						expList.addElement(newTABFiles[x].getName());
+					}
+				}
+			}
+		});
+		contentPane.add(btnLoad);
+		
+		btnRemoveBam = new JButton("Remove TAB");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnRemoveBam, 0, SpringLayout.NORTH, btnLoad);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnRemoveBam, -10, SpringLayout.EAST, contentPane);
+		btnRemoveBam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				while(listExp.getSelectedIndex() > -1) {
+					TABFiles.remove(listExp.getSelectedIndex());
+					expList.remove(listExp.getSelectedIndex());
+				}
+			}
+		});		
+		contentPane.add(btnRemoveBam);
+		
+		btnCalculate = new JButton("Calculate");
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane, -130, SpringLayout.NORTH, btnCalculate);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnCalculate, 165, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnCalculate, -165, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnCalculate, -10, SpringLayout.SOUTH, contentPane);
+		contentPane.add(btnCalculate);
+        btnCalculate.setActionCommand("start");
+        btnCalculate.addActionListener(this);
+
+		progressBar = new JProgressBar();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, progressBar, 3, SpringLayout.NORTH, btnCalculate);
+		sl_contentPane.putConstraint(SpringLayout.EAST, progressBar, -10, SpringLayout.EAST, contentPane);
+        progressBar.setStringPainted(true);
+		contentPane.add(progressBar);
+		
+        lblCurrent = new JLabel("Current Output:");
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblCurrent, 10, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, lblCurrent, -45, SpringLayout.SOUTH, contentPane);
+        lblCurrent.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+        contentPane.add(lblCurrent);
+        
+        lblDefaultToLocal = new JLabel("Default to Local Directory");
+        lblDefaultToLocal.setFont(new Font("Dialog", Font.PLAIN, 12));
+        sl_contentPane.putConstraint(SpringLayout.NORTH, lblDefaultToLocal, 1, SpringLayout.NORTH, lblCurrent);
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblDefaultToLocal, 6, SpringLayout.EAST, lblCurrent);
+        lblDefaultToLocal.setBackground(Color.WHITE);
+        contentPane.add(lblDefaultToLocal);
+        
+        btnOutput = new JButton("Output Directory");
+        sl_contentPane.putConstraint(SpringLayout.WEST, btnOutput, 0, SpringLayout.WEST, btnCalculate);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, btnOutput, -10, SpringLayout.NORTH, lblDefaultToLocal);
+        sl_contentPane.putConstraint(SpringLayout.EAST, btnOutput, -135, SpringLayout.EAST, contentPane);
+        btnOutput.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		OUTPUT_PATH = FileSelection.getOutputDir(fc);
+    			if(OUTPUT_PATH != null) {
+    				lblDefaultToLocal.setText(OUTPUT_PATH.getAbsolutePath());
+    			}
+        	}
+        });
+        contentPane.add(btnOutput);
+        
+        String[] metrics = { "Standard Pearson", "Reflective Pearson", "Spearman Rank", "Euclidean Distance", "Manhattan Distance" };
+        comboBox = new JComboBox<>(metrics);
+        contentPane.add(comboBox);
+        
+        JLabel lblSimilarityMetric = new JLabel("Similarity Metric");
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblSimilarityMetric, 60, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.NORTH, comboBox, -5, SpringLayout.NORTH, lblSimilarityMetric);
+        sl_contentPane.putConstraint(SpringLayout.WEST, comboBox, 41, SpringLayout.EAST, lblSimilarityMetric);
+        contentPane.add(lblSimilarityMetric);
+        
+        rdbtnCorrelateColumns = new JRadioButton("Correlate Columns");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, lblSimilarityMetric, 10, SpringLayout.SOUTH, rdbtnCorrelateColumns);
+        sl_contentPane.putConstraint(SpringLayout.WEST, rdbtnCorrelateColumns, 60, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.NORTH, rdbtnCorrelateColumns, 10, SpringLayout.SOUTH, scrollPane);
+        contentPane.add(rdbtnCorrelateColumns);
+        
+        rdbtnCorrelateRows = new JRadioButton("Correlate Rows");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, rdbtnCorrelateRows, 10, SpringLayout.SOUTH, scrollPane);
+        sl_contentPane.putConstraint(SpringLayout.WEST, rdbtnCorrelateRows, 40, SpringLayout.EAST, rdbtnCorrelateColumns);
+        contentPane.add(rdbtnCorrelateRows);
+
+        ButtonGroup CorrelateDIR = new ButtonGroup();
+        CorrelateDIR.add(rdbtnCorrelateColumns);
+        CorrelateDIR.add(rdbtnCorrelateRows);
+        rdbtnCorrelateColumns.setSelected(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		massXable(contentPane, false);
+    	setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        
+        task = new Task();
+        task.addPropertyChangeListener(this);
+        task.execute();
+	}
+	
+	/**
+     * Invoked when task's progress property changes.
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress" == evt.getPropertyName()) {
+            int progress = (Integer) evt.getNewValue();
+            progressBar.setValue(progress);
+        }
+    }
+	
+	public void massXable(Container con, boolean status) {
+		for(Component c : con.getComponents()) {
+			c.setEnabled(status);
+			if(c instanceof Container) { massXable((Container)c, status); }
+		}
+	}
+}
+
+
+	
