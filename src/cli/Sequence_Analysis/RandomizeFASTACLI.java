3edf897b0174022f3ccb156a322d4e04e161e86d
owlang <oliviawlang@gmail.com>
2020-01-09 23:02:13 -0500

    randomize-fasta CLI execute statements written
    The randomize-fasta tool was a straightforward implementation from the
    corresponding script class. I did some basic testing and possible
    further checks need to be written into the validation but it executes
    for now. The placeholder print statements were removed.

--- src/cli/Sequence_Analysis/RandomizeFASTACLI.java
+++ src/cli/Sequence_Analysis/RandomizeFASTACLI.java
@@ -14,7 +14,9 @@ import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.sql.Timestamp;
 import java.util.Date;
-	
+
+import scripts.Sequence_Analysis.FASTARandomize;
+
 /**
 	Sequence_AnalysisCLI/RandomizeFASTACLI
 */
@@ -22,17 +24,25 @@ import java.util.Date;
 		description = "Randomizes FASTA sequence for each input entry")
 public class RandomizeFASTACLI implements Callable<Integer> {
 	
-	@Parameters( index = "0", description = "")
-	private File bamFile;
+	@Parameters( index = "0", description = "the FASTA file ")
+	private File fastaFile;
 
-	@Option(names = {"-o", "--output"}, description = "specify output file ")
-	private File output = new File("output.txt");
+	@Option(names = {"-o", "--output"}, description = "specify output directory (name will be same as original with .gff ext)")
+	private File output;
 	
 	@Override
 	public Integer call() throws Exception {
-		System.out.println( ">RandomizeFASTACLI.call()" );
-// 		SEStats stat = new SEStats( bamFile, output );		
+		if( validateInput()!=0 ){ System.err.println("Invalid input. Check usage using '-h' or '--help'"); }
+		FASTARandomize.randomizeFASTA(fastaFile, output);
 		return(0);
 	}
 	
+	private Integer validateInput(){
+		// validation done within BEDtoGFF script
+		if( output!=null && !output.isDirectory() ){
+			System.err.println("Output must be a directory! Unable to specify specific name at this time.");
+			return(1);
+		}
+		return(0);
+	}
 }
