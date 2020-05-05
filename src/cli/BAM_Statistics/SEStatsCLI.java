b939a5b50c810d638ce586b2379f5f79f642bf13
owlang <oliviawlang@gmail.com>
2020-05-05 15:39:41 -0400

    SEStats-separate script from Swing output wrapper
    Split the scripts/../SEStats.java file into one of the same name and one
    in the window_interface classes as SEStatOutput. In order to do this,
    I used the util/CustomOutputStream to create a PrintStream object to the
    output swing window in SEStatOutput for the GUI while allowing the CLI
    tool to call the SEStats without instantiating any Swing objects.
    
    CLI and Window classes updated accordingly. CLI and GUI both work now
    even when jar is run on ACI.

--- src/cli/BAM_Statistics/SEStatsCLI.java
+++ src/cli/BAM_Statistics/SEStatsCLI.java
@@ -6,6 +6,7 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
+import java.util.Vector;
 
 import htsjdk.samtools.AbstractBAMFileIndex;
 import htsjdk.samtools.SAMSequenceRecord;
@@ -22,10 +23,10 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import scripts.BAM_Statistics.SEStats;
-
 	
 /**
 	BAM_StatisticsCLI/SEStatsCLI
+	//java -jar build/libs/ScriptManager-0.12.jar bam-statistics se-stat <bam.in> [-o <output.filename>]
 */
 @Command(name = "se-stat", mixinStandardHelpOptions = true,
 		description = "Output BAM Header including alignment statistics and parameters given any indexed (BAI) BAM File.")
@@ -33,18 +34,26 @@ public class SEStatsCLI implements Callable<Integer> {
 	
 	@Parameters( index = "0", description = "The BAM file whose statistics we want.")
 	private File bamFile;
-
-	@Option(names = {"-o", "--output"}, description = "Specify output file ")
-	private File output = new File("output-bam-stats.txt");
 	
-	final SamReaderFactory factory = SamReaderFactory.makeDefault().enable(SamReaderFactory.Option.INCLUDE_SOURCE_IN_RECORDS, SamReaderFactory.Option.VALIDATE_CRC_CHECKSUMS).validationStringency(ValidationStringency.SILENT);
-	SamReader reader;
-	PrintStream OUT = null;
+	@Option(names = {"-o", "--output"}, description = "Specify output file ")
+	private File output;
 	
 	@Override
 	public Integer call() throws Exception {
-		System.out.println( ">SEStatsCLI.call()" );
-// 		SEStats stat = new SEStats( bamFile, output );		
+		System.err.println( ">SEStatsCLI.call()" );
+		
+		if( validateInput()!=0 ){
+			System.err.println("Invalid input. Check usage using '-h' or '--help'");
+		}
+		
+		SEStats.getSEStats( output, bamFile, null );
+		System.err.println("Stats Calculations Complete");
+		
+		return(0);
+	}
+	
+	private Integer validateInput(){
+		// Define default behavior
 		return(0);
 	}
 	
