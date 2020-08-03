9666adf9e2c70faf455f815b8237194105035c5f
owlang <oliviawlang@gmail.com>
2020-08-03 17:38:00 -0400

    rename cli var in Peak_Analysis: genomeName
    This variable naming is more clear than genome because other tools
    expect a FASTA file as input as opposed to a genome build name.

--- src/cli/Peak_Analysis/RandomCoordinateCLI.java
+++ src/cli/Peak_Analysis/RandomCoordinateCLI.java
@@ -22,9 +22,9 @@ import scripts.Peak_Analysis.RandomCoordinate;
 public class RandomCoordinateCLI implements Callable<Integer> {
 	
 	@Parameters( index = "0", description = "reference genome [sacCer3_cegr|hg19|hg19_contigs|mm10]")
-	private String genome;
+	private String genomeName;
 	
-	@Option(names = {"-o", "--output"}, description = "specify output directory (name will be random_coordinates_<genome>_<window>bp.<ext>)")
+	@Option(names = {"-o", "--output"}, description = "Specify output directory (default = current working directory), file name will be random_coordinates_<genomeName>_<window>bp.<ext>")
 	private File output = null;
 	@Option(names = {"-f", "--gff"}, description = "file format output as GFF (default format as BED)")
 	private boolean formatIsBed = true;
@@ -37,13 +37,13 @@ public class RandomCoordinateCLI implements Callable<Integer> {
 	public Integer call() throws Exception {
 		System.err.println( ">RandomCoordinateCLI.call()" );
 		String validate = validateInput();
-		if( validate.compareTo("")!=0 ){
+		if(!validate.equals("")){
 			System.err.println( validate );
 			System.err.println("Invalid input. Check usage using '-h' or '--help'");
 			return(1);
 		}
 		
-		RandomCoordinate script_obj = new RandomCoordinate(genome, numSites, window, formatIsBed, output); 
+		RandomCoordinate script_obj = new RandomCoordinate(genomeName, numSites, window, formatIsBed, output); 
 		script_obj.execute();
 		
 		System.err.println( "Random Coordinate Generation Complete." );
@@ -54,26 +54,24 @@ public class RandomCoordinateCLI implements Callable<Integer> {
 		String r = "";
 		
 		//check input genomes are valid
-		if(genome.compareTo("sacCer3_cegr")==0 || genome.compareTo("hg19")==0 || genome.compareTo("hg19_contigs")==0 || genome.compareTo("mm10")==0 ){
-// 			System.err.println("Input genome is valid");
+		if(genomeName.equals("sacCer3_cegr") || genomeName.equals("hg19") || genomeName.equals("hg19_contigs") || genomeName.equals("mm10") ){
+// 			System.err.println("Input genomeName is valid");
 		}else{
-			r += "(!)Invalid genome selected(" +genome+ "), please select from one of the provided genomes: sacCer3_cegr, hg19, hg19_contigs, and mm10\n";
+			r += "(!)Invalid genomeName selected(" +genomeName+ "), please select from one of the provided genomes: sacCer3_cegr, hg19, hg19_contigs, and mm10\n";
 		}
+		String ext = "gff";
+		if(formatIsBed){ ext = "bed"; }
 		//set default output filename
 		if(output==null){
-			String NAME = "random_coordinates_" + genome + "_" + numSites + "sites_" + window + "bp";
-			if(formatIsBed){ output = new File(NAME + ".bed"); }
-			else{ output = new File(NAME + ".gff"); }
+			output = new File("random_coordinates_" + genomeName + "_" + numSites + "sites_" + window + "bp." + ext);
 		//check output filename is valid
 		}else{
-			String ext = "gff";
-			if(formatIsBed){ ext = "bed"; }
 			//check ext
 			try{
-				if(ext.compareTo(ExtensionFileFilter.getExtension(output))!=0){
-					r += "(!)Use \"." + ext + "\" extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) + "." + ext + "\n";
+				if(!ext.equals(ExtensionFileFilter.getExtension(output))){
+					r += "(!)Use \"." + ext.toUpperCase() + "\" extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) + "." + ext + "\n";
 				}
-			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use \"." + ext + "\" extension for output filename. Try: " + output + "." + ext + "\n"; }
+			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use \"." + ext.toUpperCase() + "\" extension for output filename. Try: " + output + "." + ext + "\n"; }
 			//check directory
 			if(output.getParent()==null){
 	// 			System.err.println("default to current directory");
--- src/cli/Peak_Analysis/TileGenomeCLI.java
+++ src/cli/Peak_Analysis/TileGenomeCLI.java
@@ -22,9 +22,9 @@ import scripts.Peak_Analysis.TileGenome;
 public class TileGenomeCLI implements Callable<Integer> {
 	
 	@Parameters( index = "0", description = "reference genome [sacCer3_cegr|hg19|hg19_contigs|mm10]")
-	private String genome;
+	private String genomeName;
 	
-	@Option(names = {"-o", "--output"}, description = "specify output directory (name will be genome_tiles_<genome>_<window>bp.<ext>)")
+	@Option(names = {"-o", "--output"}, description = "Specify output directory (default = current working directory), file name will be genome_tiles_<genomeName>_<window>bp.<ext>")
 	private File output = null;
 	@Option(names = {"-f", "--gff"}, description = "file format output as GFF (default format as BED)")
 	private boolean formatIsBed = true;
@@ -35,13 +35,13 @@ public class TileGenomeCLI implements Callable<Integer> {
 	public Integer call() throws Exception {
 		System.err.println( ">TileGenomeCLI.call()" );
 		String validate = validateInput();
-		if( validate.compareTo("")!=0 ){
+		if(!validate.equals("")){
 			System.err.println( validate );
 			System.err.println("Invalid input. Check usage using '-h' or '--help'");
 			return(1);
 		}
 		
-		TileGenome script_obj = new TileGenome(genome, window, formatIsBed, output);
+		TileGenome script_obj = new TileGenome(genomeName, window, formatIsBed, output);
 		script_obj.execute();
 		
 		System.err.println( "Genomic Tiling Complete." );
@@ -52,26 +52,24 @@ public class TileGenomeCLI implements Callable<Integer> {
 		String r = "";
 		
 		//check input genomes are valid
-		if(genome.compareTo("sacCer3_cegr")==0 || genome.compareTo("hg19")==0 || genome.compareTo("hg19_contigs")==0 || genome.compareTo("mm10")==0 ){
+		if(genomeName.equals("sacCer3_cegr") || genomeName.equals("hg19") || genomeName.equals("hg19_contigs") || genomeName.equals("mm10") ){
 // 			System.err.println("Input genome is valid");
 		}else{
-			r += "(!)Invalid genome selected(" +genome+ "), please select from one of the provided genomes: sacCer3_cegr, hg19, hg19_contigs, and mm10\n";
+			r += "(!)Invalid genomeName selected(" +genomeName+ "), please select from one of the provided genomes: sacCer3_cegr, hg19, hg19_contigs, and mm10\n";
 		}
+		String ext = "gff";
+		if(formatIsBed){ ext = "bed"; }
 		//set default output filename
 		if(output==null){
-			String NAME = genome + "_" + window + "bp";
-			if(formatIsBed){ output = new File(NAME + ".bed"); }
-			else{ output = new File(NAME + ".gff"); }
+			output = new File(genomeName + "_" + window + "bp." + ext);
 		//check output filename is valid
 		}else{
-			String ext = "gff";
-			if(formatIsBed){ ext = "bed"; }
 			//check ext
 			try{
-				if(ext.compareTo(ExtensionFileFilter.getExtension(output))!=0){
-					r += "(!)Use \"." + ext + "\" extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) + "." + ext + "\n";
+				if(!ext.equals(ExtensionFileFilter.getExtension(output))){
+					r += "(!)Use \"." + ext.toUpperCase() + "\" extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) + "." + ext + "\n";
 				}
-			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use \"." + ext + "\" extension for output filename. Try: " + output + "." + ext + "\n"; }
+			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use \"." + ext.toUpperCase() + "\" extension for output filename. Try: " + output + "." + ext + "\n"; }
 			//check directory
 			if(output.getParent()==null){
 	// 			System.err.println("default to current directory");
