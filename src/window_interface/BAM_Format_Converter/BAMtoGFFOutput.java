ce85e56326904520ce154830e909206dc7df49c8
owlang <oliviawlang@gmail.com>
2020-09-20 16:19:16 -0400

    create BAMtoGFF command line version
    Before, this tool was implemented with a JFrame script object so this
    commit separates out the JFrame elements for CLI implementation. JFrame
    elements are saved to the src/window_interface/*/BAMtoGFFOutput class
    while the calculations are kept in the src/scripts/*/BAMtoGFF class.
    
    window_interface/*Window
    	-adjust import to call *Output class
    window_interface/*Output
    	-adjust import to call script class
    	-adjust output argument when initializing script class to be
    		full filename and not just directory
    script/*
    	-strip out JFrame objects
    	-use output as filename rather than directory name
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

---
+++ src/window_interface/BAM_Format_Converter/BAMtoGFFOutput.java
@@ -0,0 +1,72 @@
+package window_interface.BAM_Format_Converter;
+
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import objects.CustomOutputStream;
+import scripts.BAM_Format_Converter.BAMtoGFF;
+
+@SuppressWarnings("serial")
+public class BAMtoGFFOutput extends JFrame {
+	private File BAM = null;
+	private File OUTPUTPATH = null;
+	private int STRAND = 0;
+	private String READ = "READ1";
+	
+	private static int PAIR = 1;
+	private static int MIN_INSERT = -9999;
+	private static int MAX_INSERT = -9999;
+
+	private JTextArea textArea;
+	
+	public BAMtoGFFOutput(File b, File o, int s, int pair_status,  int min_size, int max_size) {
+		setTitle("BAM to GFF Progress");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(150, 150, 600, 800);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		
+		BAM = b;
+		OUTPUTPATH = o;
+		STRAND = s;
+		PAIR = pair_status;
+		MIN_INSERT = min_size;
+		MAX_INSERT = max_size;
+		if(STRAND == 0) { READ = "READ1"; }
+		else if(STRAND == 1) { READ = "READ2"; }
+		else if(STRAND == 2) { READ = "COMBINED"; }
+		else if(STRAND == 3) { READ = "MIDPOINT"; }
+		else if(STRAND == 4) { READ = "FRAGMENT"; }
+	}
+	
+	public void run() throws IOException, InterruptedException {
+		//Open Output File
+		File OUT;
+		String NAME = BAM.getName().split("\\.")[0] + "_" + READ + ".gff";
+		if(OUTPUTPATH != null) {
+			OUT = new File(OUTPUTPATH.getCanonicalPath() + File.separator + NAME);
+		} else {
+			OUT = new File(NAME);
+		}
+		
+		//Call script here, pass in ps and OUT
+		PrintStream PS = new PrintStream( new CustomOutputStream(textArea) );
+		PS.println(NAME);
+		BAMtoGFF script_obj = new BAMtoGFF(BAM, OUT, STRAND, PAIR, MIN_INSERT, MAX_INSERT, PS);
+		script_obj.run();
+		
+		Thread.sleep(2000);
+		dispose();
+	}
+}
