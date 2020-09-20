ebe78cc9ba07d2df159e8c942509e54c70ab6785
owlang <oliviawlang@gmail.com>
2020-09-20 16:11:28 -0400

    create BAMtobedGraph command line version
    Before, this tool was implemented with a JFrame script object so this
    commit separates out the JFrame elements for CLI implementation. JFrame
    elements are saved to the src/window_interface/*/BAMtobedGraphOutput
    class while the calculations are kept in the src/scripts/*/BAMtobedGraph
    class.
    
    window_interface/*Window
    	-adjust import to call *Output class
    window_interface/*Output
    	-adjust import to call script class
    	-adjust output argument when initializing script class to be
    		full filename base and not just directory
    script/*
    	-strip out JFrame objects
    	-use output basename as filename rather than directory name
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

--- src/cli/BAM_Format_Converter/BAMtobedGraphCLI.java
+++ src/cli/BAM_Format_Converter/BAMtobedGraphCLI.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 import objects.ToolDescriptions;
 import util.ExtensionFileFilter;
-//import scripts.BAM_Format_Converter.BAMtobedGraph;
+import scripts.BAM_Format_Converter.BAMtobedGraph;
 
 /**
 	BAM_Format_ConverterCLI/BAMtobedGraphCLI
@@ -25,6 +25,36 @@ import util.ExtensionFileFilter;
 	exitCodeOnExecutionException = 1)
 public class BAMtobedGraphCLI implements Callable<Integer> {
 	
+	@Parameters( index = "0", description = "The BAM file from which we generate a new file.")
+	private File bamFile;
+	
+	@Option(names = {"-o", "--output"}, description = "specify output directory (name will be same as original with _<strand>.bedgraph ext)" )
+	private String outputBasename = null;
+	
+	//Read
+	@ArgGroup(exclusive = true, multiplicity = "0..1", heading = "%nSelect Read to output:%n\t@|fg(red) (select no more than one of these options)|@%n")
+	ReadType readType = new ReadType();
+	static class ReadType {
+		@Option(names = {"-1", "--read1"}, description = "output read 1 (default)")
+		boolean read1 = false;
+		@Option(names = {"-2", "--read2"}, description = "output read 2")
+		boolean read2 = false;
+		@Option(names = {"-a", "--all-reads"}, description = "output combined")
+		boolean combined = false;
+		@Option(names = {"-m", "--midpoint"}, description = "output midpoint (require PE)")
+		boolean midpoint = false;
+	}
+	
+	@Option(names = {"-p", "--mate-pair"}, description = "require proper mate pair (default not required)")
+	private boolean matePair = false;
+	@Option(names = {"-n", "--min-insert"}, description = "filter by min insert size in bp")
+	private int MIN_INSERT = -9999;
+	@Option(names = {"-x", "--max-insert"}, description = "filter by max insert size in bp")
+	private int MAX_INSERT = -9999;	
+	
+	private int STRAND = -9999;
+	private int PAIR;
+	
 	@Override
 	public Integer call() throws Exception {
 		System.err.println( ">BAMtobedGraphCLI.call()" );
@@ -35,16 +65,64 @@ public class BAMtobedGraphCLI implements Callable<Integer> {
 			System.exit(1);
 		}
 		
-		//SEStats.getSEStats( output, bamFile, null );
+		BAMtobedGraph script_obj = new BAMtobedGraph(bamFile, outputBasename, STRAND, PAIR, MIN_INSERT, MAX_INSERT, null);
+		script_obj.run();
 		
-		//System.err.println("Calculations Complete");
+		System.err.println("Conversion Complete");
 		return(0);
 	}
 	
 	private String validateInput() throws IOException {
 		String r = "";
-		//validate input here
-		//append messages to the user to `r`
+		
+		// set strand method
+		if(readType.read1)			{ STRAND=0; }
+		else if(readType.read2)		{ STRAND=1; }
+		else if(readType.combined)	{ STRAND=2; }
+		else if(readType.midpoint)	{ STRAND=3; }
+		else						{ STRAND=0; }
+		
+		//check inputs exist
+		if(!bamFile.exists()){
+			r += "(!)BAM file does not exist: " + bamFile.getName() + "\n";
+			return(r);
+		}
+		//check input extensions
+		if(!"bam".equals(ExtensionFileFilter.getExtension(bamFile))){
+			r += "(!)Is this a BAM file? Check extension: " + bamFile.getName() + "\n";
+		}
+		//check BAI exists
+		File f = new File(bamFile+".bai");
+		if(!f.exists() || f.isDirectory()){
+			r += "(!)BAI Index File does not exist for: " + bamFile.getName() + "\n";
+		}
+		//set default output filename
+		if(outputBasename==null){
+			if(STRAND==0){ outputBasename = bamFile.getName().split("\\.")[0] + "_READ1"; }
+			else if(STRAND==1){ outputBasename = bamFile.getName().split("\\.")[0] + "_READ2"; }
+			else if(STRAND==2){ outputBasename = bamFile.getName().split("\\.")[0] + "_COMBINED"; }
+			else if(STRAND==3){ outputBasename = bamFile.getName().split("\\.")[0] + "_MIDPOINT"; }
+			else if(STRAND==4){ outputBasename = bamFile.getName().split("\\.")[0] + "_FRAGMENT"; }
+			else { r += "(!)Somehow invalid STRAND!This error should never print. Check code if it does.\n"; }
+		//check output filename is valid
+		}else{
+			//no check ext
+			//check directory
+			File tmpOut = new File(outputBasename);
+			if(tmpOut.getParent()==null){
+	// 			System.err.println("default to current directory");
+			} else if(!tmpOut.getParentFile().exists()){
+				r += "(!)Check output directory exists: " + tmpOut.getParent() + "\n";
+			}
+		}
+		
+		// validate insert sizes
+		if( MIN_INSERT<0 && MIN_INSERT!=-9999 ){ r += "MIN_INSERT must be a positive integer value: " + MIN_INSERT + "\n"; }
+		if( MAX_INSERT<0 && MAX_INSERT!=-9999 ){ r += "MAX_INSERT must be a positive integer value: " + MAX_INSERT + "\n"; }
+		if( MAX_INSERT<MIN_INSERT ){ r += "MAX_INSERT must be larger/equal to MIN_INSERT: " + MIN_INSERT + "," + MAX_INSERT + "\n"; }
+		// turn pair status boolean into int
+		PAIR = matePair ? 1 : 0;
+		
 		return(r);
 	}
 }
