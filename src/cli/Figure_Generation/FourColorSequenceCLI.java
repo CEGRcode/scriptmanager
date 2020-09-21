1fa1918a9948d8c4b7b4aad0c7667325a9d467f4
owlang <oliviawlang@gmail.com>
2020-09-20 23:50:56 -0400

    create FourColor command line version
    This tool did not implement JFrame elements in the script class so the
    CLI implementation did not need to create an src/window_interface/*/
    FourColorPlotOutput class. As a result, only the cli/* file was
    affected. The script is a public static void method so super easy to
    implement.
    
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

--- src/cli/Figure_Generation/FourColorSequenceCLI.java
+++ src/cli/Figure_Generation/FourColorSequenceCLI.java
@@ -17,7 +17,7 @@ import java.util.regex.Pattern;
 
 import objects.ToolDescriptions;
 import util.ExtensionFileFilter;
-//import scripts.Figure_Generation.FourColorPlot;
+import scripts.Figure_Generation.FourColorPlot;
 
 /**
 	Figure_GenerationCLI/FourColorSequenceCLI
@@ -29,6 +29,18 @@ import util.ExtensionFileFilter;
 	exitCodeOnExecutionException = 1)
 public class FourColorSequenceCLI implements Callable<Integer> {
 	
+	@Parameters( index = "0", description = "input FASTA file of sequences to plot")
+	private File fastaFile;
+	
+	@Option(names = {"-o", "--output"}, description = "specify output filename, please use PNG extension\n(default=FASTA filename with \"_4color.png\" appended to the name in working directory of ScriptManager")
+	private File output = null;
+	@Option(names = {"-c", "--colors"}, arity = "4..5", description = "For custom colors: List colors to use for ATGC[N], in that order. Type hexadecimal string to represent colors, e.g. FF0000 is hexadecimal for red.\n(default=A-red,T-green,G-yellow,C-blue,N-gray, if only 4 colors specified, N will be set to gray)\n See <http://www.javascripter.net/faq/rgbtohex.htm> for some color options with their corresponding hex strings.")
+	private ArrayList<String> colors = null;
+	@Option(names = {"-x", "--pixel-width"}, description = "pixel width (default=1)")
+	private int pixelWidth = 1;
+	@Option(names = {"-y", "--pixel-height"}, description = "pixel height (default=1)")
+	private int pixelHeight = 1;
+	
 	@Override
 	public Integer call() throws Exception {
 		System.err.println( ">FourColorSequenceCLI.call()" );
@@ -38,17 +50,89 @@ public class FourColorSequenceCLI implements Callable<Integer> {
 			System.err.println("Invalid input. Check usage using '-h' or '--help'");
 			System.exit(1);
 		}
+		// Add colors into ArrayList
+		ArrayList<Color> ATCG_COLORS = new ArrayList<Color>(5);
+		if( colors!=null ){
+			System.err.println("size:"+ Integer.toString(ATCG_COLORS.size()));
+			for( int c=0; c<colors.size(); c++){
+				ATCG_COLORS.add(c, Color.decode("#"+colors.get(c)) );
+			}
+		}else{
+			ATCG_COLORS.add(0,new Color(208,   0,   0)); //default A-color		#D00000
+			ATCG_COLORS.add(1,new Color(  0, 208,   0)); //default T-color		#00D000
+			ATCG_COLORS.add(2,new Color(255, 180,   0)); //default G-color		#FFB400
+			ATCG_COLORS.add(3,new Color(  0,   0, 208)); //default C-color		#0000D0
+		}
+		// Set N-color to gray if only 4 colors provided as input
+		if( colors==null || colors.size()==4 ){
+			ATCG_COLORS.add(4,Color.GRAY); //default C-color
+		}
 		
-		//SEStats.getSEStats( output, bamFile, null );
+		// Generate Heatmap
+		FourColorPlot.generatePLOT(fastaFile, output, ATCG_COLORS, pixelHeight, pixelWidth);
 		
-		//System.err.println("Calculations Complete");
+		System.err.println("Image Generated.");
 		return(0);
 	}
 	
 	private String validateInput() throws IOException {
 		String r = "";
-		//validate input here
-		//append messages to the user to `r`
+		
+		//check inputs exist
+		if(!fastaFile.exists()){
+			r += "(!)FASTA file does not exist: " + fastaFile.getName() + "\n";
+			return(r);
+		}
+		//check input extensions
+		ExtensionFileFilter faFilter = new ExtensionFileFilter("fa");
+		if(!faFilter.accept(fastaFile)){
+			r += "(!)Is this a FASTA file? Check extension: " + fastaFile.getName() + "\n";
+		}
+		//set default output filename
+		if(output==null){
+			String NAME = ExtensionFileFilter.stripExtension(fastaFile);
+			output = new File(NAME + ".png");
+		//check output filename is valid
+		}else{
+			//check ext
+			try{
+				if(!"png".equals(ExtensionFileFilter.getExtension(output))){
+					r += "(!)Use PNG extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) + ".png\n";
+				}
+			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use PNG extension for output filename. Try: " + output + ".png\n"; }
+			//check directory
+			if(output.getParent()==null){
+// 				System.err.println("default to current directory");
+			} else if(!new File(output.getParent()).exists()){
+				r += "(!)Check output directory exists: " + output.getParent() + "\n";
+			}
+		}
+		
+		//check colors if custom
+		if( colors != null ){
+			//check that 4-5 provided
+			if( colors.size()!=4 && colors.size()!=5 ){
+				r += "(!)There must be 4 or 5 colors specified if using custom colors.";
+			}
+			//check that hex string is formatted properly
+			Pattern hexColorPat = Pattern.compile("#?[0-9A-Fa-f]{6}");
+			for( int c=0; c<colors.size(); c++ ){
+				String hexStr = colors.get(c);
+				Matcher m = hexColorPat.matcher( hexStr );
+				if( !m.matches() ){
+					r += "(!)Color must be formatted as a hexidecimal String.\n" + hexStr + " is not a valid hex string.\nExpected input string format: \"#?[0-9A-F]{6}\"";
+				}
+			}
+		}
+		//check pixel ranges are valid
+		if(pixelHeight<1){
+			r += "(!)Image Height must be a positive integer value! check \"-y\" flag.\"";
+		}
+		if(pixelWidth<1){
+			r += "(!)Image Width must be a positive integer value! check \"-x\" flag.\"";
+		}
+		
 		return(r);
 	}
 }
+
