4e287993abb5c289ed24d5cf52a6333ccbb77e0c
owlang <oliviawlang@gmail.com>
2020-09-21 11:38:05 -0400

    create RandomCoordinate command line version
    This tool did not implement JFrame elements in the script class so the
    CLI implementation did not need to create an src/window_interface/*/
    RandomCoordinateOutput class. As a result, no need to create an Output
    window class so fewer edits to be made:
    
    window_interface/*Window
    	-adjust output argument when initializing script class to be
    		full filename and not just directory
    script/*
    	-use output as filename rather than directory name
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

--- src/cli/Peak_Analysis/RandomCoordinateCLI.java
+++ src/cli/Peak_Analysis/RandomCoordinateCLI.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 import objects.ToolDescriptions;
 import util.ExtensionFileFilter;
-//import scripts.Peak_Analysis.RandomCoordinate;
+import scripts.Peak_Analysis.RandomCoordinate;
 
 /**
 	Peak_AnalysisCLI/RandomCoordinateCLI
@@ -24,6 +24,18 @@ import util.ExtensionFileFilter;
 	exitCodeOnExecutionException = 1)
 public class RandomCoordinateCLI implements Callable<Integer> {
 	
+	@Parameters( index = "0", description = "reference genome [sacCer3_cegr|hg19|hg19_contigs|mm10]")
+	private String genomeName;
+	
+	@Option(names = {"-o", "--output"}, description = "Specify output directory (default = current working directory), file name will be random_coordinates_<genomeName>_<window>bp.<ext>")
+	private File output = null;
+	@Option(names = {"-f", "--gff"}, description = "file format output as GFF (default format as BED)")
+	private boolean formatIsBed = true;
+	@Option(names = {"-n", "--num-sites"}, description = "number of sites (default=1000)")
+	private int numSites = 1000;
+	@Option(names = {"-w", "--window"}, description = "window size in bp (default=200)")
+	private int window = 200;
+	
 	@Override
 	public Integer call() throws Exception {
 		System.err.println( ">RandomCoordinateCLI.call()" );
@@ -34,16 +46,52 @@ public class RandomCoordinateCLI implements Callable<Integer> {
 			System.exit(1);
 		}
 		
-		//SEStats.getSEStats( output, bamFile, null );
+		RandomCoordinate script_obj = new RandomCoordinate(genomeName, numSites, window, formatIsBed, output); 
+		script_obj.execute();
 		
-		//System.err.println("Calculations Complete");
+		System.err.println( "Random Coordinate Generation Complete." );
 		return(0);
 	}
 	
 	private String validateInput() throws IOException {
 		String r = "";
-		//validate input here
-		//append messages to the user to `r`
+		
+		//check input genomes are valid
+		if(genomeName.equals("sacCer3_cegr") || genomeName.equals("hg19") || genomeName.equals("hg19_contigs") || genomeName.equals("mm10") ){
+// 			System.err.println("Input genomeName is valid");
+		}else{
+			r += "(!)Invalid genomeName selected(" +genomeName+ "), please select from one of the provided genomes: sacCer3_cegr, hg19, hg19_contigs, and mm10\n";
+		}
+		String ext = "gff";
+		if(formatIsBed){ ext = "bed"; }
+		//set default output filename
+		if(output==null){
+			output = new File("random_coordinates_" + genomeName + "_" + numSites + "sites_" + window + "bp." + ext);
+		//check output filename is valid
+		}else{
+			//check ext
+			try{
+				if(!ext.equals(ExtensionFileFilter.getExtension(output))){
+					r += "(!)Use \"." + ext.toUpperCase() + "\" extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) + "." + ext + "\n";
+				}
+			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use \"." + ext.toUpperCase() + "\" extension for output filename. Try: " + output + "." + ext + "\n"; }
+			//check directory
+			if(output.getParent()==null){
+	// 			System.err.println("default to current directory");
+			} else if(!new File(output.getParent()).exists()){
+				r += "(!)Check output directory exists: " + output.getParent() + "\n";
+			}
+		}
+		
+		//check number of sites
+		if( numSites<1 ){
+			r += "(!)Number of sites needs to be a positive integer.\n";
+		}
+		//check window size
+		if( window<1 ){
+			r += "(!)Window size needs to be a positive integer.\n";
+		}
+		
 		return(r);
 	}
 }
