1faa4fb94f41d8c7d53640830a24ae73c00d24ce
owlang <oliviawlang@gmail.com>
2020-09-21 00:20:32 -0400

    create MergeHeatMap command line version
    Before, this tool was implemented with a JFrame script object so this
    commit separates out the JFrame elements for CLI implementation. JFrame
    elements are saved to the src/window_interface/*/MergeHeatMapOutput
    class while the calculations are kept in the src/scripts/*/MergeHeatMap
    class.
    
    window_interface/*Window
    	-adjust import to call *Output class
    window_interface/*Output
    	-adjust import to call script class
    	-adjust output argument when initializing script class to be
    		full filename and not just directory
    script/*
    	-strip out JFrame objects
    	-use output as filename rather than directory name
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

--- src/cli/Figure_Generation/MergeHeatMapCLI.java
+++ src/cli/Figure_Generation/MergeHeatMapCLI.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Callable;
 
 import objects.ToolDescriptions;
 import util.ExtensionFileFilter;
-//import scripts.Figure_Generation.MergeHeatMapPlot;
+import scripts.Figure_Generation.MergeHeatMapPlot;
 
 /**
 	Figure_GenerationCLI/MergeHeatMapCLI
@@ -24,6 +24,14 @@ import util.ExtensionFileFilter;
 	exitCodeOnExecutionException = 1)
 public class MergeHeatMapCLI implements Callable<Integer> {
 	
+	@Parameters( index = "0", description = "First(sense) PNG heatmap to merge, input1")
+	private File senseFile;
+	@Parameters( index = "1", description = "Second(anti) PNG heatmap to merge, input2")
+	private File antiFile;
+	
+	@Option(names = {"-o", "--output"}, description = "specify output filename, please use PNG extension\n(default=\"<senseFile>_merged.png\" appended to the name in working directory of ScriptManager")
+	private File output = null;
+	
 	@Override
 	public Integer call() throws Exception {
 		System.err.println( ">MergeHeatMapCLI.call()" );
@@ -34,16 +42,52 @@ public class MergeHeatMapCLI implements Callable<Integer> {
 			System.exit(1);
 		}
 		
-		//SEStats.getSEStats( output, bamFile, null );
-		
-		//System.err.println("Calculations Complete");
+		// Generate Merged HeatMap
+		MergeHeatMapPlot.mergePNG( senseFile, antiFile, output );
+				
+		System.err.println("Image Generated.");
 		return(0);
 	}
 	
 	private String validateInput() throws IOException {
 		String r = "";
-		//validate input here
-		//append messages to the user to `r`
+		
+		//check inputs exist
+		if(!senseFile.exists()){
+			r += "(!)INPUT1 file does not exist: " + senseFile.getName() +  "\n";
+		}
+		if(!antiFile.exists()){
+			r += "(!)INPUT2 file does not exist: " + antiFile.getName() +  "\n";
+		}
+		if(!r.equals("")){ return(r); }
+		//check input extensions
+		if(!"png".equals(ExtensionFileFilter.getExtension(senseFile))){
+			r += "(!)Is this a PNG file? Check extension: " + senseFile.getName() +  "\n";
+		}
+		if(!"png".equals(ExtensionFileFilter.getExtension(antiFile))){
+			r += "(!)Is this a PNG file? Check extension: " + antiFile.getName() +  "\n";
+		}
+		//set default output filename
+		if(output==null){
+			String NAME = ExtensionFileFilter.stripExtension(senseFile);
+			output = new File(NAME + "_merged.png");
+		//check output filename is valid
+		}else{
+			//check ext
+			try{
+				if(!"png".equals(ExtensionFileFilter.getExtension(output))){
+					r += "(!)Use PNG extension for output filename. Try: " + ExtensionFileFilter.stripExtension(output) +  ".png\n";
+				}
+			} catch( NullPointerException e){ r += "(!)Output filename must have extension: use PNG extension for output filename. Try: " + output +  ".png\n"; }
+			//check directory
+			if(output.getParent()==null){
+// 				System.err.println("default to current directory");
+			} else if(!new File(output.getParent()).exists()){
+				r += "(!)Check output directory exists: " + output.getParent() + "\n";
+			}
+		}
+		
 		return(r);
 	}
+	
 }
