9a100e29b82fd7ae8a1c13044a907e069b7c67b7
owlang <oliviawlang@gmail.com>
2020-09-20 21:28:51 -0400

    create ScalingFactor command line version
    Before, this tool was implemented with a JFrame script object so this
    commit separates out the JFrame elements for CLI implementation. JFrame
    elements are saved to the src/window_interface/*/ScalingFactorOutput
    class while the calculations are kept in the src/scripts/*/ScalingFactor
    class.
    
    Note this tool's NCIS method may need deeper testing for other cases
    that would break the CLI.
    
    window_interface/*Window
    	-adjust import to call *Output class
    window_interface/*Output
    	-adjust import to call script class
    	-adjust output argument when initializing script class to be
    		full filename and not just directory
    script/*
    	-strip out JFrame objects
    	-use output as filename rather than directory name
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

--- src/cli/Read_Analysis/ScalingFactorCLI.java
+++ src/cli/Read_Analysis/ScalingFactorCLI.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 import objects.ToolDescriptions;
 import util.ExtensionFileFilter;
-//import scripts.Read_Analysis.ScalingFactor;
+import scripts.Read_Analysis.ScalingFactor;
 
 /**
 	Read_AnalysisCLI/ScalingFactorCLI
@@ -25,6 +25,35 @@ import util.ExtensionFileFilter;
 	exitCodeOnExecutionException = 1)
 public class ScalingFactorCLI implements Callable<Integer> {
 	
+	@Parameters( index = "0", description = "The BAM file from which we calculate the scaling factor.")
+	private File bamFile;
+	
+	@Option(names = {"-o", "--output"}, description = "Specify basename for output files (default = <bamFilename>_ScalingFactors.out")
+	private String outputBasename = null;
+	@Option(names = {"-f", "--blacklist"}, description = "specify blacklist file to filter by")
+	private File blacklistFilter = null;
+	@Option(names = {"-c", "--control"}, description = "control BAM file")
+	private File controlBAM = null;
+	
+	@ArgGroup(exclusive = true, heading = "Scale Options%n")
+	ScaleType scale = new ScaleType();
+	static class ScaleType{
+		@Option(names = {"-t", "--total-tag"}, description = "total tag scaling (default)")
+		private boolean total = false;
+		@Option(names = {"-n", "--ncis"}, description = "ncis normalization with window size in bp and unitless minimum fraction (default-size=500, default-fraction=0.75)" +
+		"%nAccording to the NCIS method from Liang & Keles (BMC Bioinf 2012). Also sets a background proportion estimate for the signal channel.")
+		private boolean ncis = false;
+		@Option(names = {"-b", "--both"}, description = "ncis with total tag (default-size=500, default-fraction=0.75)")
+		private boolean both = false;
+	}
+	
+	@Option(names = {"-w", "--window-size"}, description = "window size for NCIS-related scaling types (default=500)")
+	private int window = 500;
+	@Option(names = {"-m", "--min-fraction"}, description = "minimum fraction for NCIS-related scaling types (default=0.75)")
+	private double minFrac = 0.75;
+	
+	private int scaleType = 1;
+	
 	@Override
 	public Integer call() throws Exception {
 		System.err.println( ">ScalingFactorCLI.call()" );
@@ -34,17 +63,84 @@ public class ScalingFactorCLI implements Callable<Integer> {
 			System.err.println("Invalid input. Check usage using '-h' or '--help'");
 			System.exit(1);
 		}
+		System.err.println( "OUTBASE: " + outputBasename );
 		
-		//SEStats.getSEStats( output, bamFile, null );
+		ScalingFactor script_obj = new ScalingFactor(bamFile, blacklistFilter, controlBAM, outputBasename, true, scaleType, window, minFrac);
+		script_obj.run();
 		
-		//System.err.println("Calculations Complete");
+		System.err.println("Scaling Factor Calculated.");
 		return(0);
 	}
 	
 	private String validateInput() throws IOException {
 		String r = "";
-		//validate input here
-		//append messages to the user to `r`
+		
+		//check inputs exist
+		if(!bamFile.exists() && bamFile!=null){
+			r += "(!)BAM(input) file does not exist: " + bamFile.getName() + "\n";
+		}
+		if(blacklistFilter!=null){
+			if(!blacklistFilter.exists()){ r += "(!)Blacklist file does not exist: " + blacklistFilter.getName() + "\n"; }
+		}
+		if(controlBAM!=null){
+			if(!controlBAM.exists()){ r += "(!)BAM(control) file does not exist: " + controlBAM.getName() + "\n"; }
+		}
+		if(!r.equals("")){ return(r); }
+		//check input extensions & BAI exists
+		if(!"bam".equals(ExtensionFileFilter.getExtension(bamFile))){
+			r += "(!)Is this a BAM(input) file? Check extension: " + bamFile.getName() + "\n";
+		}
+		File f = new File(bamFile+".bai");
+		if(!f.exists() || f.isDirectory()){
+			r += "(!)BAI Index File does not exist for: " + bamFile.getName() + "\n";
+		}
+		if(blacklistFilter!=null){
+			if(!"bed".equals(ExtensionFileFilter.getExtension(blacklistFilter))){
+				r += "(!)Is this a BED file? Check extension: " + blacklistFilter.getName() + "\n";
+			}
+		}
+		if(controlBAM!=null){
+			if(!"bam".equals(ExtensionFileFilter.getExtension(controlBAM))){
+				r += "(!)Is this a BAM(control) file? Check extension: " + controlBAM.getName() + "\n";
+			}
+			f = new File(controlBAM+".bai");
+			if(!f.exists() || f.isDirectory()){
+				r += "(!)BAI Index File does not exist for: " + controlBAM.getName() + "\n";
+			}
+		}
+		//set default output filename
+		if(outputBasename==null){
+			outputBasename = ExtensionFileFilter.stripExtension(bamFile);
+		//check output filename is valid
+		}else{
+			File tempOut = new File(outputBasename);
+			//no check ext
+			//check directory
+			if(tempOut.getParent()==null){
+// 				System.err.println("default to current directory");
+			} else if(!new File(tempOut.getParent()).exists()){
+				r += "(!)Check output directory exists: " + tempOut.getParent() + "\n";
+			}
+		}
+		
+		//Set numeric indicator for scaling method
+		if(scale.ncis) { scaleType = 2; }
+		else if(scale.both) { scaleType = 3; }
+		
+		//NCIS-related methods must have a control file
+		if(controlBAM==null && scaleType!=1){
+			r += "(!)Control file must be given when using NCIS-related scaling methods. Unused for total-tag scaling.\n";
+		}
+		
+		//validate window
+		if(window<1){
+			r += "(!)Window must be a positive non-zero integer\n";
+		}
+		//validate minimum fraction
+		if(minFrac>1 || minFrac<0){
+			r += "(!)Minimum fraction must be between 0 and 1\n";
+		}
+		
 		return(r);
 	}
 }
