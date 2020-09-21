8650b5dc5ce3974c45cbf6103b6283f7f06653d2
owlang <oliviawlang@gmail.com>
2020-09-20 21:16:20 -0400

    create ScaleMatrix command line version
    This tool did not implement JFrame elements in the script class so the
    CLI implementation did not need to create an src/window_interface/*/
    ScaleMatrixOutput class. As a result, fewer files were affected:
    
    window_interface/*Window
    	-adjust output argument when initializing script class to be
    		full filename and not just directory
    script/*
    	-use output as filename rather than directory name
    cli/*
    	-call appropriate script class
    	-write up picocli parsing objects
    	-write up validateInput() method to check inputs and write
    		messages for the user

--- src/cli/Read_Analysis/ScaleMatrixCLI.java
+++ src/cli/Read_Analysis/ScaleMatrixCLI.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 import objects.ToolDescriptions;
 import util.ExtensionFileFilter;
-//import scripts.Read_Analysis.ScaleMatrix;
+import scripts.Read_Analysis.ScaleMatrix;
 
 /**
 	Read_AnalysisCLI/ScaleMatrixCLI
@@ -24,6 +24,19 @@ import util.ExtensionFileFilter;
 	exitCodeOnExecutionException = 1)
 public class ScaleMatrixCLI implements Callable<Integer> {
 	
+	@Parameters( index = "0", description = "Input matrix file")
+	private File matrixFile;
+	
+	@Option(names = {"-o", "--output"}, description = "Specify output file (default = <matrixFilename>_SCALE.tab)")
+	private File output = null;
+	@Option(names = {"-s", "--scaling-factor"}, description = "scaling factor (default=1)")
+	private double scale = 1;
+	@Option(names = {"-r", "--start-row"}, description = "")
+	private int startROW = 1;
+	@Option(names = {"-l", "--start-col"}, description = "")
+	private int startCOL = 2;
+	
+	
 	@Override
 	public Integer call() throws Exception {
 		System.err.println( ">ScaleMatrixCLI.call()" );
@@ -32,18 +45,44 @@ public class ScaleMatrixCLI implements Callable<Integer> {
 			System.err.println( validate );
 			System.err.println("Invalid input. Check usage using '-h' or '--help'");
 			System.exit(1);
-		}
+		}		
 		
-		//SEStats.getSEStats( output, bamFile, null );
+		ScaleMatrix script_obj = new ScaleMatrix(matrixFile, output, scale, startROW, startCOL);
+		script_obj.run();
 		
-		//System.err.println("Calculations Complete");
+		System.err.println("All Matrices Scaled.");
 		return(0);
 	}
 	
 	private String validateInput() throws IOException {
 		String r = "";
-		//validate input here
-		//append messages to the user to `r`
+		
+		//check inputs exist
+		if(!matrixFile.exists()){
+			r += "(!)MATRIX file does not exist: " + matrixFile.getName() + "\n";
+			return(r);
+		}
+		//no check ext
+		//set default output filename
+		if(output==null){
+			output = new File(ExtensionFileFilter.stripExtension(matrixFile) + "_SCALE." + ExtensionFileFilter.getExtension(matrixFile)); 
+		//check output filename is valid
+		}else if( output.isDirectory() ){
+			r += "(!)Must indicate file (not a directory) for your output.";
+		} else {
+			//no check ext
+			//check directory
+			if(output.getParent()==null){
+// 				System.err.println("default to current directory");
+			} else if(!new File(output.getParent()).exists()){
+				r += "(!)Check output directory exists: " + output.getParent() + "\n";
+			}
+		}
+		
+		//validate row&column start indexes
+		if(startROW<0){ r += "(!)Row start must not be less than zero\n"; }
+		if(startCOL<0){ r += "(!)Column start must not be less than zero\n"; }
+		
 		return(r);
 	}
 }
