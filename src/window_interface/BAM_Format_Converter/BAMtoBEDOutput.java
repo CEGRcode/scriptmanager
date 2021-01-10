567db84842cfa7c2651eafbac433e78a3939f425
owlang <oliviawlang@gmail.com>
2021-01-10 10:34:53 -0500

    cleanup whitespace formatting of bam-format tools
    Switch to a tab-delimited formatting for the BAM Format Converter tools.

--- src/window_interface/BAM_Format_Converter/BAMtoBEDOutput.java
+++ src/window_interface/BAM_Format_Converter/BAMtoBEDOutput.java
@@ -18,54 +18,60 @@ public class BAMtoBEDOutput extends JFrame {
 	private File OUTPUTPATH = null;
 	private int STRAND = 0;
 	private String READ = "READ1";
-	
+
 	private static int PAIR = 1;
 	private static int MIN_INSERT = -9999;
 	private static int MAX_INSERT = -9999;
 
 	private JTextArea textArea;
-	
-	public BAMtoBEDOutput(File b, File o, int s, int pair_status,  int min_size, int max_size) {
+
+	public BAMtoBEDOutput(File b, File o, int s, int pair_status, int min_size, int max_size) {
 		setTitle("BAM to BED Progress");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(150, 150, 600, 800);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
+
 		textArea = new JTextArea();
 		textArea.setEditable(false);
 		scrollPane.setViewportView(textArea);
-		
+
 		BAM = b;
 		OUTPUTPATH = o;
 		STRAND = s;
 		PAIR = pair_status;
 		MIN_INSERT = min_size;
 		MAX_INSERT = max_size;
-		if(STRAND == 0) { READ = "READ1"; }
-		else if(STRAND == 1) { READ = "READ2"; }
-		else if(STRAND == 2) { READ = "COMBINED"; }
-		else if(STRAND == 3) { READ = "MIDPOINT"; }
-		else if(STRAND == 4) { READ = "FRAGMENT"; }
+		if (STRAND == 0) {
+			READ = "READ1";
+		} else if (STRAND == 1) {
+			READ = "READ2";
+		} else if (STRAND == 2) {
+			READ = "COMBINED";
+		} else if (STRAND == 3) {
+			READ = "MIDPOINT";
+		} else if (STRAND == 4) {
+			READ = "FRAGMENT";
+		}
 	}
-	
+
 	public void run() throws IOException, InterruptedException {
-		//Open Output File
+		// Open Output File
 		File OUT;
 		String NAME = BAM.getName().split("\\.")[0] + "_" + READ + ".bed";
-		if(OUTPUTPATH != null) {
+		if (OUTPUTPATH != null) {
 			OUT = new File(OUTPUTPATH.getCanonicalPath() + File.separator + NAME);
 		} else {
 			OUT = new File(NAME);
 		}
-		
-		//Call script here, pass in ps and OUT
-		PrintStream PS = new PrintStream( new CustomOutputStream(textArea) );
+
+		// Call script here, pass in ps and OUT
+		PrintStream PS = new PrintStream(new CustomOutputStream(textArea));
 		PS.println(NAME);
 		BAMtoBED script_obj = new BAMtoBED(BAM, OUT, STRAND, PAIR, MIN_INSERT, MAX_INSERT, PS);
 		script_obj.run();
-		
+
 		Thread.sleep(2000);
 		dispose();
 	}
--- src/window_interface/BAM_Format_Converter/BAMtoscIDXOutput.java
+++ src/window_interface/BAM_Format_Converter/BAMtoscIDXOutput.java
@@ -18,53 +18,58 @@ public class BAMtoscIDXOutput extends JFrame {
 	private File OUTPUTPATH = null;
 	private int STRAND = 0;
 	private String READ = "READ1";
-	
+
 	private static int PAIR = 1;
 	private static int MIN_INSERT = -9999;
 	private static int MAX_INSERT = -9999;
 
 	private JTextArea textArea;
-	
+
 	public BAMtoscIDXOutput(File b, File o, int s, int pair_status, int min_size, int max_size) {
 		setTitle("BAM to scIDX Progress");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(150, 150, 600, 800);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
+
 		textArea = new JTextArea();
 		textArea.setEditable(false);
 		scrollPane.setViewportView(textArea);
-		
+
 		BAM = b;
 		OUTPUTPATH = o;
 		STRAND = s;
 		PAIR = pair_status;
 		MIN_INSERT = min_size;
 		MAX_INSERT = max_size;
-		if(STRAND == 0) { READ = "READ1"; }
-		else if(STRAND == 1) { READ = "READ2"; }
-		else if(STRAND == 2) { READ = "COMBINED"; }
-		else if(STRAND == 3) { READ = "MIDPOINT"; }
+		if (STRAND == 0) {
+			READ = "READ1";
+		} else if (STRAND == 1) {
+			READ = "READ2";
+		} else if (STRAND == 2) {
+			READ = "COMBINED";
+		} else if (STRAND == 3) {
+			READ = "MIDPOINT";
+		}
 	}
-	
+
 	public void run() throws IOException, InterruptedException {
-		//Open Output File
+		// Open Output File
 		File OUT;
 		String NAME = BAM.getName().split("\\.")[0] + "_" + READ + ".tab";
-		if(OUTPUTPATH != null) {
+		if (OUTPUTPATH != null) {
 			OUT = new File(OUTPUTPATH.getCanonicalPath() + File.separator + NAME);
 		} else {
 			OUT = new File(NAME);
 		}
-		
-		//Call script here, pass in ps and OUT
-		PrintStream PS = new PrintStream( new CustomOutputStream(textArea) );
+
+		// Call script here, pass in ps and OUT
+		PrintStream PS = new PrintStream(new CustomOutputStream(textArea));
 		PS.println(NAME);
 		BAMtoscIDX script_obj = new BAMtoscIDX(BAM, OUT, STRAND, PAIR, MIN_INSERT, MAX_INSERT, PS);
 		script_obj.run();
-		
+
 		Thread.sleep(2000);
 		dispose();
 	}
