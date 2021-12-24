0d300ece39709f54db80147da4d272d0dde4a8f3
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2021-12-24 08:09:42 -0500

    reset default BED coord strand parsing
    Invert if statement int TagPileup so that it parses BED coordinates such that unexpected strand characters default to the positive strand ("+").

--- src/scripts/Read_Analysis/TagPileup.java
+++ src/scripts/Read_Analysis/TagPileup.java
@@ -378,16 +378,13 @@ public class TagPileup {
 					}
 					if (Integer.parseInt(temp[1]) >= 0) {
 						if (temp.length > 4) {
-							if (temp[5].equals("+")) {
-								COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]),
-										"+", name));
+							if (temp[5].equals("-")) {
+								COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "-", name));
 							} else {
-								COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]),
-										"-", name));
+								COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+", name));
 							}
 						} else {
-							COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+",
-									name));
+							COORD.add(new BEDCoord(temp[0], Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), "+", name));
 						}
 
 					} else {
