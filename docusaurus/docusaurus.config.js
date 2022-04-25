de1bfd32afd4d25276b2ba1339fd3e89c7e6bddf
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2022-04-25 14:14:38 -0400

    Fix docusaurus preset key error
    The "tools" key must be deprecated from whichever initialization was used for the original docusaurus creation. Either way, it appears clearing this out removes the build error when running `npm run docusaurus build` and may fix the docusaurus deployment issue.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -144,12 +144,6 @@ module.exports = {
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
         },
-        tools: {
-            sidebarPath: require.resolve('./sidebars.js'),
-            // Please change this to your repo.
-            editUrl:
-              'https://github.com/facebook/docusaurus/edit/master/website/',
-          },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
