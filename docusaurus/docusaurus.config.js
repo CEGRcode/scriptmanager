dd16896576a73c77dba531a2e68f054506743f44
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2022-04-29 15:32:13 -0400

    remove blog and footer of docusaurus
    The docusaurus.config.js file was cleaned up to remove some superfluous commented code, the docusaurus blog information, and the footer. Github links were fixed to point to the CEGRcode original repo (rather than the owlang fork) and other missed placeholder values were filled with ScriptManager-specific code. The markdown file pointers were changed in syntax to match Stencil docusaurus instance ("to" and "activeBasePath" vs "docID"). Hopefully the changes made will fix the docusaurus deployment issues. Docusaurs builds and serves locally without issues.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,3 +1,4 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'ScriptManager',
   tagline: 'Toolbox for analyzing your genomic datasets',
@@ -12,126 +13,28 @@ module.exports = {
     navbar: {
       title: 'ScriptManager',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'ScriptManager',
         src: 'img/ScriptManagerTempLogo_AgencyFBfont.png', //svg file was here
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
+          type: 'doc',
+          docId: 'quick-start',
           label: 'Docs',
           position: 'left',
         },
         {
-          to: 'docs/read-analysis/tag-pileup',
+          type: 'doc',
+          docId: 'read-analysis/tag-pileup',
           label: 'Tools',
           position: 'left'
         },
-        // {
-        //   to: 'docs/bam-format-converter/bam-to-scidx',
-        //   label: 'BAM Format Converter',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/bam-manipulation/sort-bam',
-        //   label: 'BAM Manipulation',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/bam-statistics/se-stat',
-        //   label: 'BAM Statistics',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/coordinate-manipulation/expand-bed',
-        //   label: 'Coordinate Manipulation',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/figure-generation/heatmap',
-        //   label: 'Figure Generation',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/file-utilities/md5checksum',
-        //   label: 'File Utilities',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/peak-analysis/peak-align-ref',
-        //   label: 'Peak Analysis',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/read-analysis/tag-pileup',
-        //   label: 'Read Analysis',
-        //   position: 'left'
-        // },
-        // {
-        //   to: 'docs/sequence-analysis/fasta-extract',
-        //   label: 'Sequence Analysis',
-        //   position: 'left'
-        // },
-        {
-          to: 'blog',
-          label: 'Blog',
-          position: 'left'
-        },
-        {
-          href: 'https://github.com/owlang/scriptmanager',
+        { 
+          href: 'https://github.com/CEGRcode/scriptmanager',
           label: 'GitHub',
           position: 'right',
         },
       ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Tool Index (A-Z)',
-              to: 'docs/tool-index',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/CEGRcode/scriptmanager',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -143,12 +46,6 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
