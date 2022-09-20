53862ec935139762c8a66688f1b9137e2986d065
owlang <oliviawlang@gmail.com>
2022-09-20 00:59:13 -0400

    add gzip support for in/out of ExpandBED
    Add support for gzipped input BED files for the ExpandBED script. #91
    
    - Decorated classes with JavaDocs tags
    - Reorganized argument validation in ExpandBEDCLI to both support -z flag and clarify output filename logic
    - Update BufferedReader and output stream objects to detect and support Gzipped files
    - add input validation logic to script
    - Update ExpandBED window with an "Output GZIP" checkbox that is added to the script call
    - update file selector to include gzipped file extensions
    - change "Convert" to "Execute" buttons
    - Reorganize default output filepath determination logic

--- src/scripts/Coordinate_Manipulation/BED_Manipulation/ExpandBED.java
+++ src/scripts/Coordinate_Manipulation/BED_Manipulation/ExpandBED.java
@@ -1,21 +1,57 @@
 package scripts.Coordinate_Manipulation.BED_Manipulation;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
+import util.GZipUtilities;
+
+/**
+ * Class that contains method for expanding (BED) coordinate intervals from the center/border by a user-defined direction and distance.
+ * 
+ * @author William KM Lai
+ *
+ */
 public class ExpandBED {
-	public static void expandBEDBorders(File out_filepath, File input, int SIZE, boolean ExCenter) throws IOException {
-
-		Scanner scan = new Scanner(input);
+	/**
+	 * Self-contained method for expanding the BED-formatted intervals in a BED file by user-specified distance and strategy. This method accounts for even-sized BED interval expansion in midpoint calculations by using the strand-aware downstream nucleotide between the two center nucleotides.
+	 * 
+	 * @param out_filepath Filepath to save expanded BED-formatted files. If null, outputs to STDOUT.
+	 * @param input Filepath to starting BED-formatted coordinates we want to shift. Supports automatic detection and handling of GZipped BED-formatted files. Must have at least 3 tab-delimited columns per BED specifications.
+	 * @param SIZE Integer value indicating number of nucleotides to expand by (must be a positive integer).
+	 * @param ExCenter Specifies expansion strategy: if true, size expansion will be performed from the midpoint of each BED interval, if false, size expansion will be performed from the border/edges of the BED intervals (default=true).
+	 * @param gzOutput If this is true, the output file will be gzipped.
+	 * @throws IOException
+	 */
+	public static void expandBEDBorders(File out_filepath, File input, int SIZE, boolean ExCenter, boolean gzOutput ) throws IOException {
+		// Initialize output writer
 		PrintStream OUT = System.out;
-		if (out_filepath != null)
-			OUT = new PrintStream(out_filepath);
-
-		while (scan.hasNextLine()) {
-			String[] temp = scan.nextLine().split("\t");
+		if (out_filepath != null) {
+			if (gzOutput) {
+				OUT = new PrintStream(new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(out_filepath))));
+			} else {
+				OUT = new PrintStream(new BufferedOutputStream(new FileOutputStream(out_filepath)));
+			}
+		}
+		// Check if file is gzipped and instantiate appropriate BufferedReader
+		BufferedReader br;
+		if(GZipUtilities.isGZipped(input)) {
+			br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(input)), "UTF-8"));
+		} else {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(input), "UTF-8"));
+		}
+		// Initialize line variable to loop through
+		String line = br.readLine();
+		while (line != null) {
+			String[] temp = line.split("\t");
 			if (temp[0].toLowerCase().contains("track") || temp[0].startsWith("#")) {
 				OUT.println(String.join("\t", temp));
 			} else {
@@ -41,12 +77,15 @@ public class ExpandBED {
 						}
 						OUT.println();
 					} else {
-						System.out.println("Invalid Coordinate in File!!!\n" + Arrays.toString(temp));
+						System.out.println("Invalid Coordinate in File!!! (coordinate must be >= 0)\n" + Arrays.toString(temp));
 					}
+				} else {
+					System.out.println("Invalid Coordinate in File!!! (must have at least 3 columns)\n" + Arrays.toString(temp));
 				}
 			}
+			line = br.readLine();
 		}
-		scan.close();
+		br.close();
 		OUT.close();
 	}
 }
