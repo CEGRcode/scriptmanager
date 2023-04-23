90494ba7e7f4acd4554ee39069845541b732a56b
owlang <oliviawlang@gmail.com>
2023-04-23 14:31:07 -0400

    fix typo in Tool Descriptions
    fix typo in signal duplication (signal_dup_description) constant

--- src/main/java/scriptmanager/objects/ToolDescriptions.java
+++ src/main/java/scriptmanager/objects/ToolDescriptions.java
@@ -59,7 +59,7 @@ public class ToolDescriptions {
 	public static final String filter_bed_description = "Filter BED file using user-specified exclusion zone using the score column to determine which peak to retain.";
 	public static final String tile_genome_description = "Generate a coordinate file that tiles (non-overlapping) across an entire genome.";
 	public static final String rand_coord_description = "Generate random BED coordinates based on reference genome.";
-	public static final String signal_dup_description = "Calculate duplication statistics at user-specified regsions.";
+	public static final String signal_dup_description = "Calculate duplication statistics at user-specified regions.";
 
 	// Coordinate Manipulation
 	public static final String expand_bed_description = "Expand BED file given user-defined criteria."; //"Expands input BED file by adding positions to the border or around the center"
