5d91c0dbbff602c008330ba511fa1fcece9c3dc6
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2025-10-08 10:57:02 -0400

    Update BAMUtilities.java

--- src/main/java/scriptmanager/util/BAMUtilities.java
+++ src/main/java/scriptmanager/util/BAMUtilities.java
@@ -151,24 +151,21 @@ public class BAMUtilities {
 	/**
 	 * Calculates the standardization ratio for a given BAM file
 	 * @param BAM BAM file used to calculate ratio
-	 * @param read Read Type (1 = Read1, 1 = Read2, 3 = All reads)
+	 * @parm p store read aspect and read output encodings
 	 * @return The standardization ratio for a given BAM file
 	 * @throws IOException Invalid file or parameters
 	 * @throws ScriptManagerException 
+	 * @throws OptionException 
 	 */
-	public static double calculateStandardizationRatio(File BAM, int read) throws IOException {
+	public static double calculateStandardizationRatio(File BAM, PileupParameters p) throws IOException, OptionException, ScriptManagerException {
 		// Get Genome Size
 		double totalGenome = getGenomeSize(BAM);
-
 		// Get total aligned
-		//System.out.println("Genome Size: " + totalGenome + "\nTotal tags: " + totalAligned + "\nDetected Read 1: " + READ1 + "\nDetected Read 2: " + READ2 + "\nDetected Midpoints: " + MID);
-		if(read == 0) { totalAligned = READ1; }
-		else if(read == 1) { totalAligned = READ2; }
-		else if(read == 2) { totalAligned = READ1 + READ2; }
-		else if(read == 3) { totalAligned = MID; }
-
+		double totalAligned = getReadCount(BAM, p);
+		// Divide for ratio
 		if(totalAligned > 0) { return (totalGenome / totalAligned); }
-		else { return 1; }
+		// Return default
+		return 1;
 	}
 
 	/**
